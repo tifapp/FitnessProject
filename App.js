361fa35742523e63dcbb8a3821463bc3bbfd8ae0
sean <seanim0920@gmail.com>
2020-08-30 16:57:05 -0700

    base login form

--- App.js
+++ App.js
@@ -1,15 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, Button, Image, View, TextInput } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default function App() {	
+	const [username, setUsername] = useState('');
+	const [password, setPassword] = useState('');
+	
+	const readValues = () => {
+		if (username == "Sean" && password == "pass") {
+			alert('yes');
+		} else {
+			alert('no');
+		}
+	}
+
+	return (
+	  <View>
+		<Text>Login Screen</Text>
+		
+		<TextInput
+		  style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+		  placeholder={"username"}
+          onChangeText={(text) => setUsername(text)}
+		  value={username}
+		/>
+		<TextInput
+		  style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+		  placeholder={"password"}
+          onChangeText={(text) => setPassword(text)}
+		  value={password}
+		/>
+		<Button
+			title="Submit"
+			onPress={readValues}
+		/>
+	  </View>
+	);
 }
+
 
 const styles = StyleSheet.create({
   container: {
