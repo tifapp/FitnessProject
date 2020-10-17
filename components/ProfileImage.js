00bbef3032a04ba845d31a3db701251fdffcecee
sean <seanim0920@gmail.com>
2020-10-17 00:24:17 -0700

    A loading icon will now display when someone's profile image is loading. Removed conflicting file in pinpoint and usenotification functions.

--- components/ProfileImage.js
+++ components/ProfileImage.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { View, StyleSheet, Image, TouchableOpacity, Alert } from 'react-native';
+import { View, StyleSheet, Image, TouchableOpacity, Alert, ActivityIndicator, } from 'react-native';
 import { Storage } from "aws-amplify";
 
 var styles = require('../styles/stylesheet');
@@ -8,6 +8,7 @@ export const ProfileImage = (props) => { //user is required in props. it's a typ
     const [imageURL, setImageURL] = useState('');
 
     useEffect(() => {
+        setImageURL('loading...');
         Storage.get('profileimage.jpg', { level: 'protected', identityId: props.user.identityId }) //this will incur lots of repeated calls to the backend, idk how else to fix it right now
             .then((imageURL) => { //console.log("found profile image!", imageURL); 
                 Image.getSize(imageURL, () => {
@@ -19,10 +20,18 @@ export const ProfileImage = (props) => { //user is required in props. it's a typ
             .catch((err) => { console.log("could not find image!", err) }) //should just use a "profilepic" component
     }, []);
 
-    return (
-        <Image 
-            style={[props.style]}
-            source={imageURL === '' ? require('../assets/icon.png') : { uri: imageURL }}
-        />
-    )
+    if (imageURL == 'loading...') {
+        return (
+            <ActivityIndicator
+                style={[props.style]}
+            />
+        )
+    } else {
+        return (
+            <Image 
+                style={[props.style]}
+                source={imageURL === '' ? require('../assets/icon.png') : { uri: imageURL }}
+            />
+        )
+    }
 }
