f726f130a39348aba82c18023eba0fdf271ae641
sean <seanim0920@gmail.com>
2020-11-06 14:52:12 -0800

    The user's profile pic is stored in the cache with a higher priority than other profile pics so it doesn't get kicked out of the cache ProfileImage's caching seems to work.

--- components/ProfileImage.js
+++ components/ProfileImage.js
@@ -4,37 +4,38 @@ import { Cache, Storage } from "aws-amplify";
 
 var styles = require('../styles/stylesheet');
 
+//currently the predicted behavior is that it will cache images but the links will be invalid after 15 minutes-1 hour. let's see.
+
 export const ProfileImage = (props) => { //user is required in props. it's a type of object described in userschema.graphql
     const [imageURL, setImageURL] = useState('');
 
     const addURLtoCache = () => {
         console.log('cache missed!'); //this isn't printing for some reason
-        setImageURL('loading...');
         //console.log("this identity id is...", props.user.identityId); //seems to fix the profile images on the feed screen, idk why
         Storage.get('profileimage.jpg', { level: 'protected', identityId: props.user.identityId }) //this will incur lots of repeated calls to the backend, idk how else to fix it right now
             .then((imageURL) => { //console.log("found profile image!", imageURL); 
                 Image.getSize(imageURL, () => {
-                    if (mounted) {
-                        Cache.setItem(props.user.id, imageURL);
+                    //if (mounted) {
+                        Cache.setItem(props.user.id, imageURL, { expires: Date.now().getTime() + 86400000 });
                         setImageURL(imageURL);
-                    }
+                    //}
                 }, err => {
-                    if (mounted) {
-                        Cache.setItem(props.user.id, '');
+                    //if (mounted) {
+                        Cache.setItem(props.user.id, '', { expires: Date.now().getTime() + 86400000 });
                         setImageURL('');
-                    }
+                    //}
                 });
             })
             .catch((err) => { console.log("could not find image!", err) }) //should just use a "profilepic" component
+        return 'loading...';
     }
 
     useEffect(() => {
-        let mounted = true; //to prevent the "unmounted update" warning
         Cache.getItem(props.user.id, { callback: addURLtoCache }) //we'll check if this user's profile image url was stored in the cache, if not we'll look for it
         .then((url)=>{
-            console.log(url);
+            console.log('cache hit! ', url.substring(0, 15), '...');
             setImageURL(url)});
-        return () => mounted = false;
+        //return () => mounted = false;
     }, [props.user]);
 
     if (imageURL == 'loading...') {
@@ -45,10 +46,10 @@ export const ProfileImage = (props) => { //user is required in props. it's a typ
             />
         )
     } else {
-        return ( //source={imageURL === '' ? require('../assets/icon.png') : { uri: imageURL }}
+        return (
             <Image 
                 style={[props.style]}
-                source={require('../assets/icon.png')}
+                source={imageURL === '' ? require('../assets/icon.png') : { uri: imageURL }}
             />
         )
     }
