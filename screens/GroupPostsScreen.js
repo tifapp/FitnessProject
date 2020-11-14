e8eb7ee35c80ae7bea8f05036183aec7fc04b788
skolhatk <skolhatk@gmail.com>
2020-11-13 20:57:29 -0800

    Update GroupPostsScreen.js

--- screens/GroupPostsScreen.js
+++ screens/GroupPostsScreen.js
@@ -18,16 +18,19 @@ var styles = require('styles/stylesheet');
 
 export default function GroupPostsScreen({ navigation, route }) {
   const { group } = route.params;
-  console.log(route.params);
+  //console.log(route.params);
   //console.log(route);
   const goEditGroupScreen = () => {
-    navigation.navigate('Create Group', {group: group});
+    navigation.navigate('Create Group', {group: group, check: true});
   }
   return (
     <View style={styles.containerStyle}>
+
+      {route.params.id == group.userID ? 
       <TouchableOpacity onPress ={goEditGroupScreen}>
         <AntDesign style = {{alignSelf: "flex-end", marginTop: 10, marginRight: 15}} name="edit" size={30} color="black" />
-      </TouchableOpacity>
+      </TouchableOpacity> : 
+      null}
       
       <View style={styles.header}>
         
