aafcd30eaff0bcd956caf6de3deaf5016b9d558d
sean <seanim0920@gmail.com>
2020-11-17 23:07:16 -0800

    finally got the thumbnail generation code to work successfully. now we just need to display this thumbnail on the app.

--- amplify/backend/function/S3Triggeref91c1ec/src/index.js
+++ amplify/backend/function/S3Triggeref91c1ec/src/index.js
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-const gm = require('gm').subClass({ imageMagick: true })
+const Sharp = require('sharp');
 const aws = require('aws-sdk')
 const s3 = new aws.S3()
 
@@ -10,7 +9,7 @@ exports.handler = (event, context, callback) => {
   const BUCKET = event.Records[0].s3.bucket.name
 
   /* Get the image data we will use from the first record in the event object */
-  const KEY = event.Records[0].s3.object.key
+  const KEY = event.Records[0].s3.object.key.replace('%3A', ':')
   const PARTS = KEY.split('/')
 
   /* Check to see if the base folder is already set to thumbnails, if it is we return so we do not have a recursive call. */
@@ -20,69 +19,27 @@ exports.handler = (event, context, callback) => {
   /* Stores the main file name in a variable */
   let FILE = PARTS[PARTS.length - 1]
 
+  console.log("attempting to get the image with this key: ", KEY, "in this bucket: ", BUCKET);
+
   s3.getObject({ Bucket: BUCKET, Key: KEY }).promise()
     .then(image => {
-      gm(image.Body)
-        .resize(WIDTH, HEIGHT)
-        .setFormat('jpeg')
-        .toBuffer(function (err, buffer) {
-          if (err) {
-            console.log('error storing and resizing image: ', err)
-            callback(err)
-          }
-          else {
-            s3.putObject({ Bucket: BUCKET, Body: buffer, Key: `thumbnails/thumbnail-${FILE}` }).promise()
+      // Use the Sharp module to resize the image and save in a buffer.
+      Sharp(image.Body).resize(80).toBuffer()
+        .then((buffer) => {
+          s3.putObject({ Bucket: BUCKET, Body: buffer, Key: `thumbnails/${KEY}/thumbnail-${FILE}` }).promise()
             .then(() => { callback(null) })
-            .catch(err => { callback(err) })
-          }
+            .catch(err => {
+              console.log('error storing and resizing image: ', err)
+              callback(err)
+            })
+        })
+        .catch(err => {
+          console.log('error in step 2 of resizing image: ', err)
+          callback(err)
         })
     })
     .catch(err => {
       console.log('error resizing image: ', err)
       callback(err)
     })
-=======
-const gm = require('gm').subClass({ imageMagick: true })
-const aws = require('aws-sdk')
-const s3 = new aws.S3()
-
-const WIDTH = 100
-const HEIGHT = 100
-
-exports.handler = (event, context, callback) => {
-  const BUCKET = event.Records[0].s3.bucket.name
-
-  /* Get the image data we will use from the first record in the event object */
-  const KEY = event.Records[0].s3.object.key
-  const PARTS = KEY.split('/')
-
-  /* Check to see if the base folder is already set to thumbnails, if it is we return so we do not have a recursive call. */
-  const BASE_FOLDER = PARTS[0]
-  if (BASE_FOLDER === 'thumbnails') return
-
-  /* Stores the main file name in a variable */
-  let FILE = PARTS[PARTS.length - 1]
-
-  s3.getObject({ Bucket: BUCKET, Key: KEY }).promise()
-    .then(image => {
-      gm(image.Body)
-        .resize(WIDTH, HEIGHT)
-        .setFormat('jpeg')
-        .toBuffer(function (err, buffer) {
-          if (err) {
-            console.log('error storing and resizing image: ', err)
-            callback(err)
-          }
-          else {
-            s3.putObject({ Bucket: BUCKET, Body: buffer, Key: `thumbnails/thumbnail-${FILE}` }).promise()
-            .then(() => { callback(null) })
-            .catch(err => { callback(err) })
-          }
-        })
-    })
-    .catch(err => {
-      console.log('error resizing image: ', err)
-      callback(err)
-    })
->>>>>>> development
 }
