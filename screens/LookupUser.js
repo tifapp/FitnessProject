da211de78ebc5bdfdfe56205a5d02b049e4f91b2
suryatiru <sutiruma@ucsc.edu>
2020-11-26 19:04:50 -0800

    Friend Request
    - In Lookup screen, users can send or unsend friend requests

--- screens/LookupUser.js
+++ screens/LookupUser.js
@@ -1,17 +1,84 @@
 import React, { useState, useEffect } from 'react';
 import { ActivityIndicator, View, StyleSheet, Text, ScrollView, TouchableOpacity, Alert, Button, Image, Dimensions} from 'react-native';
-import { Auth} from "aws-amplify";
+import { Auth, API, graphqlOperation} from "aws-amplify";
 import { StackActions, NavigationActions } from 'react-navigation';
 import { ProfileImage } from 'components/ProfileImage'
 import { MaterialCommunityIcons } from '@expo/vector-icons'; 
+import { createPost, updatePost, deletePost, createFriend, deleteFriend } from "root/src/graphql/mutations";
+import { getFriend } from "root/src/graphql/queries";
+
 var styles = require('styles/stylesheet');
 
 const LookupUser = ({ route, navigation }) => {
+
+    const [friendRequest, setFriendRequest] = useState(false);
 
     const { user } = route.params;
     const { id } = route.params;
 
     console.log("checking out the profile of ", user.name);
+
+    const checkFriendRequest = async () => {
+        try {
+          const friend = await API.graphql(
+            graphqlOperation(getFriend, {sender: route.params?.id, receiever: user.id})
+          );
+          console.log(friend);
+
+          if(friend.data.getFriend==null){
+            setFriendRequest(true);
+          }
+          else{
+            setFriendRequest(false);
+          }
+
+        
+        } catch (err) {
+            console.log(err);
+          //console.log("error in finding user ", err);
+         setFriendRequest(true);
+          //console.log("check");
+        }
+      };
+
+    const addFriend = async () => {
+        Alert.alert('Submitting Friend Request...', '', [], {cancelable: false})
+    
+        const newFriend = {
+            timestamp: Math.floor(Date.now()/1000),
+            sender: route.params?.id,
+            receiever: user.id,
+            accepted: false
+        };
+        setFriendRequest(false);
+    
+        try {
+          await API.graphql(graphqlOperation(createFriend, { input: newFriend }));
+          console.log("success");
+          alert('Friend request submitted successfully!');
+        } catch (err) {
+          console.log(err);
+          alert('Friend request could not be submitted! ');
+        }
+      };
+
+      const deleteFriendRequest = async () => {
+        Alert.alert('Deleting Friend Request...', '', [], {cancelable: false})
+        console.log("hello");
+        try {
+          await API.graphql(graphqlOperation(deleteFriend, { input: {sender: route.params?.id, receiever: user.id}}));
+          setFriendRequest(true);
+          console.log("success");
+        } catch(err) {
+          console.log(err);
+          console.log("error in deleting post: ");
+        }
+      };
+      
+      useEffect(() => {
+        checkFriendRequest();
+      }, []);
+      
 
     return (
         <ScrollView>
@@ -64,6 +131,25 @@ const LookupUser = ({ route, navigation }) => {
                     <Text>Goals: </Text>
                 </View>
             <Text style={styles.textBoxStyle}>{user.goals}</Text>
+            <View style={styles.buttonFormat}>
+            { friendRequest ?
+            <TouchableOpacity
+              onPress={() => {addFriend()
+              }}
+              style={styles.submitButton}
+            >
+              <Text style={styles.buttonTextStyle}>Send Friend Request</Text>
+            </TouchableOpacity>
+            :
+            <TouchableOpacity
+              onPress={() => {deleteFriendRequest()
+              }}
+              style={styles.unsendButton}
+            >
+              <Text style={styles.buttonTextStyle}>Unsend Friend Request</Text>
+            </TouchableOpacity>
+            }
+          </View>
         </ScrollView>
     )
 }
