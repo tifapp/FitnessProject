2eb66ce7383328d8e9d7e7573caf5456b1078508
sean <seanim0920@gmail.com>
2020-12-03 16:43:00 -0800

    lmbda function has been fixed again. for some reason it switched back to the windows version of sharp so I had to reinstall the linux version of sharp. then it wasn't updating so i had to add semicolons to the index.js so it would take the updated sharp version. then I had to change the base_folder variable because it was incorrect and resulting in an infinite loop earlier. now the thumbnail generation function should work.

--- amplify/backend/function/S3Triggeref91c1ec/src/index.js
+++ amplify/backend/function/S3Triggeref91c1ec/src/index.js
@@ -1,19 +1,19 @@
 const Sharp = require('sharp');
-const aws = require('aws-sdk')
-const s3 = new aws.S3()
+const aws = require('aws-sdk');
+const s3 = new aws.S3();
 
-const WIDTH = 100
-const HEIGHT = 100
+const WIDTH = 100;
+const HEIGHT = 100;
 
 exports.handler = (event, context, callback) => {
-  const BUCKET = event.Records[0].s3.bucket.name
+  const BUCKET = event.Records[0].s3.bucket.name;
 
   /* Get the image data we will use from the first record in the event object */
   const KEY = event.Records[0].s3.object.key.replace('%3A', ':')
   const PARTS = KEY.split('/')
 
   /* Check to see if the base folder is already set to thumbnails, if it is we return so we do not have a recursive call. */
-  const BASE_FOLDER = PARTS[0]
+  const BASE_FOLDER = PARTS[1];
   if (BASE_FOLDER === 'thumbnails') return
 
   /* Stores the main file name in a variable */
