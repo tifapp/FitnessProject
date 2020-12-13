5676113d2ed7522043f48908c787b19e6137283f
sean <seanim0920@gmail.com>
2020-12-13 00:35:51 -0800

    friend schema has "hifives" field. will increment when friendrequests are made after the friendship is established.

--- amplify/backend/function/connectFriendRequests/src/index.js
+++ amplify/backend/function/connectFriendRequests/src/index.js
@@ -1,12 +1,10 @@
 //let's test this out! make friend requests and see if the logs are visible on cloudwatch
 
-exports.handler = event => {
+exports.handler = (event, context) => {
   //eslint-disable-line
-  console.log(JSON.stringify(event, null, 2));
   event.Records.forEach(record => {
-    console.log(record.eventID);
-    console.log(record.eventName);
-    console.log('DynamoDB Record: %j', record.dynamodb);
+    console.log('the event is...', JSON.stringify(event));
+    console.log('the context is...', JSON.stringify(context));
   });
   return Promise.resolve('Successfully processed DynamoDB record');
 };
