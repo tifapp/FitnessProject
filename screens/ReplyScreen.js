d2d6a81ab834b8de5b7111d10127af032e75f1a6
sean <seanim0920@gmail.com>
2020-12-27 16:57:06 -0800

    preparing reply screen, a spinoff of the feed screen

---
+++ screens/ReplyScreen.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect, PureComponent } from "react";
+import {
+  Text,
+  View,
+  TouchableOpacity
+} from "react-native";
+// Get the aws resources configuration parameters
+import awsconfig from "root/aws-exports"; // if you are using Amplify CLI
+import FeedScreen from "screens/FeedScreen";
+import { Amplify } from "aws-amplify";
+import { AntDesign } from '@expo/vector-icons';
+
+Amplify.configure(awsconfig);
+
+var styles = require('styles/stylesheet');
+
+export default function ReplyScreen({ navigation, route }) {
+
+  const { op } = route.params;
+
+  return (
+    <View style={styles.containerStyle}>
+      <PostItem
+        item={item}
+        deletePostsAsync={deletePostsAsync}
+        writtenByYou={item.userId === route.params?.id}
+        setPostVal={setPostVal}
+        setUpdatePostID={setUpdatePostID}
+      />
+
+      <FeedScreen
+        navigation={navigation}
+        route={route}
+        initialParams={route.params}
+      />
+    </View>
+  );
+};
