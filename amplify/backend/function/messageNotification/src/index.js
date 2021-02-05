1225315bdc664b895773d80a0e7b9a3b1a283b70
suryatiru <sutiruma@ucsc.edu>
2021-02-05 12:20:19 -0800

    Message_Notifications
    - Users will now be notified whenver they get a new message
    
    - If they are on the Messages Tab then they wont receive a notification except for a slight ding to alert them

---
+++ amplify/backend/function/messageNotification/src/index.js
@@ -0,0 +1,282 @@
+/*
+const {
+  Expo
+} = require("expo-server-sdk");
+
+// Create a new Expo SDK client
+let expo = new Expo();
+
+require('isomorphic-fetch');
+const AWS = require('aws-sdk/global');
+const AUTH_TYPE = require('aws-appsync').AUTH_TYPE;
+const AWSAppSyncClient = require('aws-appsync').default;
+const gql = require('graphql-tag');
+
+
+exports.handler = event => {
+  //eslint-disable-line
+  console.log(JSON.stringify(event, null, 2));
+  event.Records.forEach(record => {
+    console.log(record.eventID);
+    console.log(record.eventName);
+    console.log('DynamoDB Record: %j', record.dynamodb);
+  });
+  return Promise.resolve('Successfully processed DynamoDB record');
+};
+*/
+
+const {
+  Expo
+} = require("expo-server-sdk");
+
+// Create a new Expo SDK client
+let expo = new Expo();
+
+require('isomorphic-fetch');
+const AWS = require('aws-sdk/global');
+const AUTH_TYPE = require('aws-appsync').AUTH_TYPE;
+const AWSAppSyncClient = require('aws-appsync').default;
+const gql = require('graphql-tag');
+const { addNotificationReceivedListener } = require("expo-notifications");
+
+const config = {
+  url: process.env.API_FITNESSPROJECTAPI_GRAPHQLAPIENDPOINTOUTPUT,
+  region: process.env.AWS_REGION,
+  auth: {
+    type: AUTH_TYPE.AWS_IAM,
+    credentials: AWS.config.credentials,
+  },
+  disableOffline: true
+};
+
+const client = new AWSAppSyncClient(config);
+
+const getFriendRequest =
+  `
+  query GetFriendRequest($sender: ID!, $receiver: ID!) {
+    getFriendRequest(sender: $sender, receiver: $receiver) {
+      createdAt
+      updatedAt
+      sender
+      receiver
+    }
+  }
+`;
+
+const getFriendship =
+  `
+  query GetFriendship($user1: ID!, $user2: ID!) {
+    getFriendship(user1: $user1, user2: $user2) {
+      createdAt
+      updatedAt
+      user1
+      user2
+      hifives
+    }
+  }
+`;
+
+const updateFriendship =
+  `
+  mutation UpdateFriendship(
+    $input: UpdateFriendshipInput!
+    $condition: ModelFriendshipConditionInput
+  ) {
+    updateFriendship(input: $input, condition: $condition) {
+      createdAt
+      updatedAt
+      user1
+      user2
+      hifives
+    }
+  }
+`;
+
+const deleteFriendRequest =
+  `
+  mutation DeleteFriendRequest(
+    $input: DeleteFriendRequestInput!
+    $condition: ModelFriendRequestConditionInput
+  ) {
+    deleteFriendRequest(input: $input, condition: $condition) {
+      sender
+      receiver
+      createdAt
+      updatedAt
+    }
+  }
+`;
+
+const createFriendship =
+  `
+  mutation CreateFriendship(
+    $input: CreateFriendshipInput!
+    $condition: ModelFriendshipConditionInput
+  ) {
+    createFriendship(input: $input, condition: $condition) {
+      createdAt
+      updatedAt
+      user1
+      user2
+      hifives
+    }
+  }
+`;
+
+const getUser =
+  `
+  query GetUser($id: ID!) {
+    getUser(id: $id) {
+      id
+      identityId
+      name
+      age
+      gender
+      bio
+      goals
+      latitude
+      longitude
+      deviceToken
+      createdAt
+      updatedAt
+    }
+  }
+`;
+
+const getPost =
+  `
+  query GetPost($createdAt: AWSDateTime!, $userId: ID!) {
+    getPost(createdAt: $createdAt, userId: $userId) {
+      createdAt
+      updatedAt
+      userId
+      description
+      parentId
+      channel
+      receiver
+      isParent
+    }
+  }
+  `;
+
+const postsByParentId =
+  `
+  query PostsByParentId(
+    $parentId: String
+    $isParent: ModelIntKeyConditionInput
+    $sortDirection: ModelSortDirection
+    $filter: ModelPostFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    postsByParentId(
+      parentId: $parentId
+      isParent: $isParent
+      sortDirection: $sortDirection
+      filter: $filter
+      limit: $limit
+      nextToken: $nextToken
+    ) {
+      items {
+        createdAt
+        updatedAt
+        userId
+        description
+        parentId
+        channel
+        receiver
+        isParent
+      }
+      nextToken
+    }
+  }
+  `;
+
+async function sendNotification(deviceToken, message) {
+  if (deviceToken == null || deviceToken == '') return;
+
+  console.log("creating notification");
+  const pushMessage = {
+    to: String(deviceToken),
+    sound: "default",
+    body: message,
+    data: { "status": "ok" }
+  };
+  try {
+    let tickets = await expo.sendPushNotificationsAsync([pushMessage]);
+    // NOTE: If a ticket contains an error code in ticket.details.error, you
+    // must handle it appropriately. The error codes are listed in the Expo
+    // documentation:
+    // https://docs.expo.io/versions/latest/guides/push-notifications#response-format
+    async () => {
+      // Like sending notifications, there are different strategies you could use
+      // to retrieve batches of receipts from the Expo service.
+      try {
+        let receipts = await expo.getPushNotificationReceiptsAsync(tickets[0].id);
+        console.log(receipts);
+
+        // The receipts specify whether Apple or Google successfully received the
+        // notification and information about an error, if one occurred.
+        for (let receipt of receipts) {
+          if (receipt.status === "ok") {
+            continue;
+          } else if (receipt.status === "error") {
+            console.error(
+              `There was an error sending a notification: ${receipt.message}`
+            );
+            if (receipt.details && receipt.details.error) {
+              // The error codes are listed in the Expo documentation:
+              // https://docs.expo.io/versions/latest/guides/push-notifications#response-format
+              // You must handle the errors appropriately.
+              console.error(`The error code is ${receipt.details.error}`);
+            }
+          }
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+exports.handler = async(event, context, callback) => {
+  event.Records.forEach((record) => {
+    if (record.eventName == "INSERT") {
+      (async () => {
+        try {
+         console.log("---------------------------------------------------------");
+         const receiver = record.dynamodb.NewImage.receiver.S;
+         const sender = record.dynamodb.NewImage.sender.S;
+
+         if(receiver == null){
+           return;
+         }
+
+         const receiverName = await client.query({
+          query: gql(getUser),
+          variables: {
+            id: receiver
+          }
+        });
+
+        const senderName = await client.query({
+          query: gql(getUser),
+          variables: {
+            id: sender
+          }
+        });
+
+          //if (friendshipcheck.data.getFriendship != null) {
+            await sendNotification(receiverName.data.getUser.deviceToken, senderName.data.getUser.name + " sent you a message!"); //truncate the sender's name!
+            console.log("Finished Messaging");
+          //}
+        }
+        catch (e) {
+          console.warn('Error sending reply: ', e);
+        }
+      })();
+    }
+  });
+};
