f16e9bdab028a90f889e830d42691880ea8d7372
sean <seanim0920@gmail.com>
2021-02-17 16:59:52 -0800

    no message

--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -253,7 +253,7 @@ export const listLikes = /* GraphQL */ `
 export const postsByChannel = /* GraphQL */ `
   query PostsByChannel(
     $channel: ID
-    $parentId: ModelStringKeyConditionInput
+    $parentIdIsParentCreatedAt: ModelPostByChannelCompositeKeyConditionInput
     $sortDirection: ModelSortDirection
     $filter: ModelPostFilterInput
     $limit: Int
@@ -261,7 +261,7 @@ export const postsByChannel = /* GraphQL */ `
   ) {
     postsByChannel(
       channel: $channel
-      parentId: $parentId
+      parentIdIsParentCreatedAt: $parentIdIsParentCreatedAt
       sortDirection: $sortDirection
       filter: $filter
       limit: $limit
