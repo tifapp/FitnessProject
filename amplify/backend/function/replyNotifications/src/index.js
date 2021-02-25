fae286d6a5e06d4502e7d1ca1507e8faeeeeb1d9
sean <seanim0920@gmail.com>
2021-02-24 19:17:38 -0800

    put the message notification code in the reply notification function and deleted the messagenotification function because it was giving us problems

--- amplify/backend/function/replyNotifications/src/index.js
+++ amplify/backend/function/replyNotifications/src/index.js
@@ -218,73 +218,87 @@ exports.handler = (event, context, callback) => {
     if (record.eventName == "INSERT") {
       (async () => {
         try {
-          console.log("---------------------------------------------------------");
-          //console.log('getting a new object with a sender of ', JSON.stringify(record.dynamodb.NewImage.sender.S), 'seeing if an object exists with that receiver');
-          //const receiver = record.dynamodb.NewImage.receiver.S;
-          //const sender = record.dynamodb.NewImage.sender.S;
-
-          /*
-          const result = await client.query({
-            query: gql(getFriendRequest),
-            variables: {
-              sender: receiver,
-              receiver: sender,
-            }
-          });
-          */
-          const parentId = record.dynamodb.NewImage.parentId.S;
-          const childId = record.dynamodb.NewImage.userId.S;
-
-          console.log(record.dynamodb.NewImage);
-
-          if (record.dynamodb.NewImage.isParent.N == 1) {
-            callback(null, "Not a reply");
-            return;
+          if (record.dynamodb.NewImage.receiver != null) {
+            //message notifications
+            console.log("the new post is indeed a message")
+            const receiver = record.dynamodb.NewImage.receiver.S;
+            const sender = record.dynamodb.NewImage.userId.S;
+            
+            console.log("successfully saved receiver and sender variables")
+  
+            const receiverName = await client.query({
+              query: gql(getUser),
+              variables: {
+                id: receiver
+              }
+            });
+  
+            const senderName = await client.query({
+              query: gql(getUser),
+              variables: {
+                id: sender
+              }
+            });
+  
+            console.log(receiverName.data.getUser)
+            console.log(senderName.data.getUser)
+  
+            await sendNotification(receiverName.data.getUser.deviceToken, senderName.data.getUser.name + " sent you a message!"); //truncate the sender's name!
+            console.log("sent notifications finished")
+            callback(null, "Successfully sent messaging notification");
+          } else if (record.dynamodb.NewImage.isParent.N == 0) {
+            //reply notifications
+            const parentId = record.dynamodb.NewImage.parentId.S;
+            const childId = record.dynamodb.NewImage.userId.S;
+  
+            console.log(record.dynamodb.NewImage);
+  
+            const parents = await client.query({
+              query: gql(postsByParentId),
+              variables: {
+                parentId: parentId,
+                sortDirection: 'DESC',
+                limit: 1
+              }
+            });
+  
+            const uniqueParentNames = parents.data.postsByParentId.items[0];
+  
+            console.log("*************************************************");
+            console.log(uniqueParentNames);
+            console.log("*************************************************");
+  
+            const childPost = await client.query({
+              query: gql(getUser),
+              variables: {
+                id: childId
+              }
+            });
+  
+            console.log("fetched child post");
+  
+            const parentPost = await client.query({
+              query: gql(getUser),
+              variables: {
+                id: uniqueParentNames.userId
+              }
+            });
+  
+            // const friendshipcheck = await client.query({
+            //   query: gql(getFriendship),
+            //   variables: {
+            //     user1: childId < uniqueParentNames.userId ? childId : uniqueParentNames.userId,
+            //     user2: childId < uniqueParentNames.userId ? uniqueParentNames.userId : childId,
+            //   }
+            // });
+  
+            //if (friendshipcheck.data.getFriendship != null) {
+              await sendNotification(parentPost.data.getUser.deviceToken, childPost.data.getUser.name + " sent you a reply!"); //truncate the sender's name!
+              callback(null, "Finished Replying");
+            //}
+          } else {
+            callback(null, "not a message or reply");
           }
-
-          const parents = await client.query({
-            query: gql(postsByParentId),
-            variables: {
-              parentId: parentId,
-              sortDirection: 'DESC',
-              limit: 1
-            }
-          });
-
-          const uniqueParentNames = parents.data.postsByParentId.items[0];
-
-          console.log("*************************************************");
-          console.log(uniqueParentNames);
-          console.log("*************************************************");
-
-          const childPost = await client.query({
-            query: gql(getUser),
-            variables: {
-              id: childId
-            }
-          });
-
-          console.log("fetched child post");
-
-          const parentPost = await client.query({
-            query: gql(getUser),
-            variables: {
-              id: uniqueParentNames.userId
-            }
-          });
-
-          // const friendshipcheck = await client.query({
-          //   query: gql(getFriendship),
-          //   variables: {
-          //     user1: childId < uniqueParentNames.userId ? childId : uniqueParentNames.userId,
-          //     user2: childId < uniqueParentNames.userId ? uniqueParentNames.userId : childId,
-          //   }
-          // });
-
-          //if (friendshipcheck.data.getFriendship != null) {
-            await sendNotification(parentPost.data.getUser.deviceToken, childPost.data.getUser.name + " sent you a reply!"); //truncate the sender's name!
-            callback(null, "Finished Replying");
-          //}
         }
         catch (e) {
           console.warn('Error sending reply: ', e);
