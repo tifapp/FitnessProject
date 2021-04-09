5ac565135b4322a4d3faff4fc5349258c671e57b
suryatiru <sutiruma@ucsc.edu>
2021-04-09 14:01:30 -0700

    Conversations
    - Setup a list of conversations in the ConversationScreen.js

--- screens/ConversationScreen.js
+++ screens/ConversationScreen.js
@@ -8,12 +8,59 @@ import {
   useRef, 
   PureComponent
 } from "react-native";
+import { conversationsByLastUpdated } from "../src/graphql/queries";
 
 var styles = require('styles/stylesheet');
 
 export default function ConversationScreen({ navigation, route }) {
+
+  const [conversations, setConversations] = useState([]);
+
   return (
-    <View style={styles.containerStyle}>  
+    <View style={styles.containerStyle}>
+      <View>
+          <SafeAreaView style={{ flex: 1 }}>
+            <APIList
+              initialAmount={10}
+              additionalAmount={20}
+              horizontal={true}
+              queryOperation={conversationsByLastUpdated}
+              data={conversations}
+              setDataFunction={setConversations}
+              renderItem={({ item }) => (
+                <View style={{ marginVertical: 5 }}>
+                  <View
+                    style={{
+                      flexDirection: "row",
+                      alignSelf: "center",
+                      marginVertical: 5,
+                      justifyContent: "space-between",
+                      width: "80%",
+                    }}
+                  >
+                    <ProfileImageAndName
+                      vertical={true}
+                      imageStyle={[
+                        styles.smallImageStyle,
+                        { marginHorizontal: 20 },
+                      ]}
+                      userId={item}
+                    />
+
+                    <Text>
+                      {conversations.users}
+                    </Text>
+
+                    <Text>
+                      {conversations.lastMessage}
+                    </Text>
+                  </View>
+                </View>
+              )}
+              keyExtractor={(item) => item}
+            />
+          </SafeAreaView>
+        </View>  
     </View>
   );
 };
