4bf2c5754cf6f1e71fe07523c13a7afe702d9482
sean <seanim0920@gmail.com>
2021-05-16 00:12:08 -0700

    tried updating the deletereplies function so it would delete likes as well. attempted to remove a seemingly unnecessary await at the end to see if that would still enable the function to run and make it a bit faster.

--- amplify/backend/function/deleteReplies/src/index.js
+++ amplify/backend/function/deleteReplies/src/index.js
@@ -7,14 +7,50 @@ Amplify Params - DO NOT EDIT */
 require('isomorphic-fetch');
 const gql = require('graphql-tag');
 
-const { postsByParentId } = require('/opt/queries');
-const { deletePost } = require('/opt/mutations');
+const { postsByParentId, likesByPost } = require('/opt/queries');
+const { deletePost, deleteLike } = require('/opt/mutations');
 const { client } = require('/opt/backendResources');
 
 exports.handler = (event, context, callback) => {
   event.Records.forEach((record) => {
     if (record.eventName == "REMOVE") {
       const parentId = record.dynamodb.OldImage.parentId.S;
+
+      const likes = record.dynamodb.OldImage.likes.N;
+      const createdAt = record.dynamodb.OldImage.createdAt.S;
+      const userId = record.dynamodb.OldImage.userId.S;
+      (async () => {
+        try {
+          while (likes > 0) {
+            const results = await client.query({
+              query: gql(likesByPost),
+              variables: {
+                postId: createdAt + "#" + userId,
+              }
+            });
+
+            likes = likes - results.length
+
+            results.data.likesByPost.items.map((like) => {
+              client.mutate({
+                mutation: gql(deleteLike),
+                variables: {
+                  input: {
+                    userId: like.userId,
+                    postId: like.postId
+                  }
+                }
+              });
+            });
+          }
+
+          callback(null, "successfully deleted likes");
+        } catch (e) {
+          console.warn('Error deleting likes: ', e);
+          callback(Error(e));
+        }
+      })();
+
       if (record.dynamodb.OldImage.isParent.N == 1) {
         (async () => {
           try {
@@ -25,7 +61,7 @@ exports.handler = (event, context, callback) => {
               }
             });
 
-            await Promise.all(results.data.postsByParentId.items.map(async (post) => {
+            results.data.postsByParentId.items.map((post) => {
               client.mutate({
                 mutation: gql(deletePost),
                 variables: {
@@ -35,14 +71,12 @@ exports.handler = (event, context, callback) => {
                   }
                 }
               });
-            }));
+            });
 
             callback(null, "successfully deleted replies");
-            return;
           } catch (e) {
             console.warn('Error deleting replies: ', e);
             callback(Error(e));
-            return;
           }
         })();
       }
