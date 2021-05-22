a96f7838a833fbbf293df982a09988ea5bde58da
sean <seanim0920@gmail.com>
2021-05-22 16:10:07 -0700

    deleting your user should also delete all your friends

--- amplify/backend/function/deleteUserData/src/index.js
+++ amplify/backend/function/deleteUserData/src/index.js
@@ -1,8 +1,8 @@
 require('isomorphic-fetch');
 const gql = require('graphql-tag');
 
-const { postsByUser } = require('/opt/queries');
-const { deletePost } = require('/opt/mutations');
+const { postsByUser, listFriendships, friendsByReceiver } = require('/opt/queries');
+const { deletePost, deleteFriendship } = require('/opt/mutations');
 const { client } = require('/opt/backendResources');
 
 exports.handler = event => {
@@ -30,6 +30,70 @@ exports.handler = event => {
                   input: {
                     userId: post.userId,
                     createdAt: post.createdAt
+                  }
+                }
+              });
+            });
+          }
+
+          callback(null, "successfully deleted posts");
+        } catch (e) {
+          console.warn('Error deleting posts: ', e);
+          callback(Error(e));
+        }
+      })();
+      
+      (async () => {
+        try {
+          while (true) {
+            const results = await client.query({
+              query: gql(listFriendships),
+              variables: {
+                sender: userId,
+              }
+            });
+
+            if (results.length <= 0) break;
+
+            results.data.listFriendships.items.map((friendship) => {
+              client.mutate({
+                mutation: gql(deleteFriendship),
+                variables: {
+                  input: {
+                    sender: friendship.sender,
+                    receiver: friendship.receiver
+                  }
+                }
+              });
+            });
+          }
+
+          callback(null, "successfully deleted posts");
+        } catch (e) {
+          console.warn('Error deleting posts: ', e);
+          callback(Error(e));
+        }
+      })();
+      
+      (async () => {
+        try {
+          while (true) {
+            const results = await client.query({
+              query: gql(friendsByReceiver),
+              variables: {
+                receiver: userId,
+              }
+            });
+
+            if (results.length <= 0) break;
+
+            results.data.friendsByReceiver.items.map((friendship) => {
+              client.mutate({
+                mutation: gql(deleteFriendship),
+                variables: {
+                  input: {
+                    sender: friendship.sender,
+                    receiver: friendship.receiver
                   }
                 }
               });
