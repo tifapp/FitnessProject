49ab82d94b7e0d10e9fa942f8a053d467a146257
sean <seanim0920@gmail.com>
2021-09-17 01:35:44 -0700

    made settings screen with privacy settings

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,69 +1,18 @@
 import React, { useState, useEffect, useRef } from "react";
 import {
-  ActivityIndicator,
   View,
-  StyleSheet,
   Text,
-  ScrollView,
   TouchableOpacity,
   Alert,
-  Image,
-  Linking,
+  Switch,
   LayoutAnimation
 } from "react-native";
-import ProfilePic from "components/ProfileImagePicker";
-import BasicInfo from "components/basicInfoComponents/BasicInfo";
-import DetailedInfo from "components/detailedInfoComponents/DetailedInfo";
-import useUserDatabase from "hooks/useUserDatabase";
-import { Auth } from "aws-amplify";
-import { Platform } from "react-native";
-import { Ionicons } from "@expo/vector-icons";
-import CheckBox from "@react-native-community/checkbox"; //when ios is supported, we'll use this
-import getLocation from "hooks/useLocation";
-import APIList from "components/APIList";
-import { ProfileImageAndName } from "components/ProfileImageAndName";
-import { blocksByDate } from "root/src/graphql/queries";
-import { deleteBlock } from "root/src/graphql/mutations";
-import printTime from "hooks/printTime";
+import { updateUser } from "root/src/graphql/mutations";
 import { API, graphqlOperation } from "aws-amplify";
 
 var styles = require("styles/stylesheet");
 
 const SettingsScreen = ({ navigation, route }) => {
-  const [isOptionsOpen, setIsOptionsOpen] = useState(false);
-  const [blockList, setBlockList] = useState([]);
-  
-  const alertOptions = {
-    cancelable: true,
-    onDismiss: () => setIsOptionsOpen(false),
-  };
-
-  const unblock = async (blockeeId) => {
-    const title = "Are you sure you want to unblock this friend?";
-    const options = [
-      {
-        text: "Yes",
-        onPress: () => {
-          console.log("about to delete this user: ", blockeeId)
-          global.localBlockList = global.localBlockList.filter((item) => item.blockee != blockeeId);
-          LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
-          setBlockList(blockList.filter((item) => item.blockee != blockeeId));
-          API.graphql(
-            graphqlOperation(deleteBlock, { input: { userId: route.params?.myId, blockee: blockeeId} })
-          );
-        },
-      },
-      {
-        text: "Cancel",
-        type: "cancel",
-        onPress: () => {
-          setIsOptionsOpen(false);
-        },
-      },
-    ];
-    Alert.alert(title, "", options, alertOptions);
-  };
-
   useEffect(() => {
     const onFocus = navigation.addListener('focus', () => {
       console.log("got to settings", global.localBlockList);
@@ -75,70 +24,95 @@ const SettingsScreen = ({ navigation, route }) => {
   }, [navigation])
 
   return (
-    <APIList
-      initialAmount={10}
-      additionalAmount={20}
-      queryOperation={blocksByDate}
-      data={blockList}
-      setDataFunction={(results) => {global.localBlockList = results, setBlockList(results)}}
-      renderItem={({ item }) => (
-        <View
+    <View>
+      <Text style={{
+        fontSize: 18,
+        color: "black",
+      }}>Privacy</Text>
+      <View style={{flexDirection: "row"}}>
+        <APISwitch
+          apicall={            
+            () => 
+            API.graphql(
+              graphqlOperation(updateUser, { input: { friendRequestPrivacy: isEnabled } })
+            )
+          }
+        />
+        <Text
           style={{
-            flexDirection: "row",
-            alignSelf: "flex-start",
-            alignItems: "center",
-            marginVertical: 5,
-            justifyContent: "space-evenly",
+            fontSize: 16,
           }}
         >
-          <ProfileImageAndName
-            imageStyle={[styles.smallImageStyle, { marginHorizontal: 20 }]}
-            userId={item.blockee}
-            
-            sibling={
-              <View
-                style={{
-                  flexDirection: "row",
-                  justifyContent: "flex-end",
-                  alignItems: "flex-end",
-                  flex: 1,
-                  marginHorizontal: 30,
-                }}
-              >
-              <Text style={{marginHorizontal: 16, color: "gray"}}>{printTime(item.createdAt)}</Text>
-    
-              <TouchableOpacity onPress={() => unblock(item.blockee, item.createdAt)}>
-                <Text>
-                  unblock
-                  {
-                    //then ask to verify the unblocking, like with friend requests
-                    //should disappear from the list when confirming, or just fade out? maybe jump to bottom with the option to reblock that person
-                  }
-                </Text>
-              </TouchableOpacity>
-              </View>
-            }
-          />
-        </View>
-      )}
-      notRefreshable={true}
-      filter={{ userId: route.params?.myId }}
-      contentContainerStyle={{ flexGrow: 1, flex: 1, justifyContent: 'center', alignContent: "center", alignItems: "center", alignSelf: "center", }}
-      keyExtractor={(item) => item.blockee}
-      ListEmptyComponent={
-        <Text
-        style={{
-          alignSelf: "center",
-          justifyContent: "center",
-          color: "gray",
-          marginVertical: 30,
-          fontSize: 15,
-        }}>
-          You haven't blocked anyone.
+          Allow strangers to message you
         </Text>
-      }
-    />
+      </View>
+      <View style={{flexDirection: "row"}}>
+        <APISwitch
+          apicall={            
+            () => 
+            API.graphql(
+              graphqlOperation(updateUser, { input: { messagesPrivacy: isEnabled } })
+            )
+          }
+        />
+        <Text
+          style={{
+            fontSize: 16,
+          }}
+        >
+          Allow strangers to send you friend requests (will still allow mutual friends)
+        </Text>
+      </View>
+    </View>
   );
 };
+function APISwitch({ apicall }) {
+  const [isEnabled, setIsEnabled] = useState(false); //should fetch from backend
+  const enabledRef = useRef();
+  const timerIsRunning = useRef();
+  const enabledTimeout = useRef();
+
+  const resetTimeout = () => {
+    //if there's already a timeout running do not update ref
+    //if there isn't, update ref
+    if (!timerIsRunning.current) {
+      enabledRef.current = isEnabled;
+    }
+    timerIsRunning.current = true;
+    clearTimeout(enabledTimeout.current);
+    enabledTimeout.current = setTimeout(sendAPICall, 1000);
+  };
+
+  const sendAPICall = () => {
+    if (isEnabled == enabledRef.current) {
+      //console.log("sent API call, hopefully debounce works.");
+      apicall();
+    }
+
+    timerIsRunning.current = false;
+  };
+
+  const toggleAsync = async () => {
+    //liked ? playSound("unlike") : playSound("like");
+    Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Heavy);
+    try {
+      setIsEnabled(!isEnabled);
+      resetTimeout();
+    } catch (err) {
+      console.log(err);
+      alert("Could not be submitted!");
+    }
+  };
+
+  return (
+    <Switch
+      trackColor={{ false: "#efefef", true: "#26c6a2" }}
+      thumbColor={isEnabled ? "#f5dd4b" : "#f4f3f4"}
+      ios_backgroundColor="#3e3e3e"
+      onValueChange={toggleAsync}
+      value={isEnabled}
+    />
+  );
+}
 
 export default SettingsScreen;
