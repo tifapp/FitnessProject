706ed948d231d984669c16b2287191fc5095fdda
suryatiru <sutiruma@ucsc.edu>
2022-03-23 23:27:03 -0700

    Added Extensions and Paths

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,25 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
+    plugins: [
+      ["@babel/plugin-transform-flow-strip-types"],
+      ["@babel/plugin-proposal-decorators", { legacy: true }],
+      ["@babel/plugin-proposal-class-properties", { loose: true }],
+      [
+        "module-resolver",
+        {
+          alias: {
+            "@screens": "./screens",
+            "@components": "./components",
+            "@stacks": "./stacks",
+            "@hooks":"./hooks",
+            "@assets":"./assets",
+            "@graphql": "./src/graphql"
+          },
+          extensions: [".js", ".jsx", ".ts", ".tsx"],
+        },
+      ],
+    ],
   };
 };
