b1f6062a9324da2477a0ee9f2e29b205eb900303
suryatiru <sutiruma@ucsc.edu>
2022-10-10 22:50:51 -0700

    Converted the common subfolder to Typescript

--- components/common/IconButton.tsx
+++ components/common/IconButton.tsx
@@ -2,6 +2,19 @@ import { MaterialIcons } from "@expo/vector-icons";
 import React from "react";
 import { Text, TouchableOpacity } from "react-native";
 
+interface Props {
+  onPress: () => {},
+  iconName: string,
+  color: string,
+  style: Object,
+  label: string,
+  size: number,
+  margin: number,
+  isLabelFirst: boolean,
+  fontSize: number,
+  fontWeight: string,
+}
+
 export default function IconButton({
   onPress = () => {},
   iconName,
