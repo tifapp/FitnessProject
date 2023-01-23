43c470bbb0f07c5c81437676e07711626a10dcdd
Matthew Hayes <matthewha853@gmail.com>
2023-01-22 20:47:40 -0800

    Setup simple test example

---
+++ tests/example.test.ts
@@ -0,0 +1,4 @@
+describe("This is an example of a simple test", () => {
+  test("1 + 1 = 2", () => expect(1 + 1).toEqual(2));
+  it("should make adding 2 to 1 equal 3", () => expect(1 + 2).toEqual(3));
+});
