109fd90fc827b3ecfcec585c4b1ad2b196565541
Matthew Hayes <matthewha853@gmail.com>
2023-01-31 22:12:11 -0800

    Remove explicit dependence of CommentsModal from PostWithSingleReplyView

--- components/postComponents/PostWithSingleReplyView.tsx
+++ components/postComponents/PostWithSingleReplyView.tsx
@@ -1,39 +1,28 @@
 import { UserPost, userPostToPost } from "../../lib/posts/UserPost";
 import { Button } from "react-native";
-import API, { graphqlOperation } from "@aws-amplify/api";
 import { useRef } from "react";
 import Modal, { ModalRefType } from "@components/common/Modal";
-import CommentsModal from "@components/postComponents/CommentsModal";
 import PostItem from "@components/PostItem";
 
 export type PostWithSingleReplyProps = {
   post: UserPost;
   reply: UserPost;
+  fullRepliesView: (post: UserPost) => JSX.Element;
 };
 
-const PostWithSingleReplyView = ({ post, reply }: PostWithSingleReplyProps) => {
+const PostWithSingleReplyView = ({
+  post,
+  reply,
+  fullRepliesView,
+}: PostWithSingleReplyProps) => {
   const repliesModalRef = useRef<ModalRefType>(null);
   return (
     <>
-      <PostItem
-        item={userPostToPost(post)}
-        likes={post.likesCount}
-        reportPost={async () => 1}
-        operations={{ removeItem: () => {}, replaceItem: () => {} }}
-        writtenByYou={post.writtenByYou}
-        isVisible={false}
-        shouldSubscribe={true}
-      />
       <Button
         title="View All"
         onPress={() => repliesModalRef.current?.showModal()}
       />
-      <Modal ref={repliesModalRef}>
-        <CommentsModal
-          item={userPostToPost(post)}
-          operations={{ removeItem: () => {}, replaceItem: () => {} }}
-        />
-      </Modal>
+      <Modal ref={repliesModalRef}>{fullRepliesView(post)}</Modal>
     </>
   );
 };
--- tests/posts/PostWithSingleReplyView.test.tsx
+++ tests/posts/PostWithSingleReplyView.test.tsx
@@ -3,13 +3,14 @@ import { screen, fireEvent, render } from "@testing-library/react-native";
 import { UserPost, TestUserPosts } from "../../lib/posts/UserPost";
 import { NavigationContainer } from "@react-navigation/native";
 import { createDrawerNavigator } from "@react-navigation/drawer";
+import { Text } from "react-native";
 
 describe("Post With Single Reply tests", () => {
-  it("can navigate to the full comments thread for a post", async () => {
+  it("can navigate to the full replies thread for a post", async () => {
     renderPostWithReply(TestUserPosts.writtenByYou, TestUserPosts.blob);
-    expectCommentsThreadToNotBeLoaded();
-    navigateToCommentsThread();
-    await expectCommentsThreadDidLoad();
+    expectFullRepliesThreadToNotBeLoaded();
+    navigateToFullReplies();
+    await expectFullRepliesThreadDidLoad();
   });
 });
 
@@ -20,21 +21,29 @@ const renderPostWithReply = (post: UserPost, reply: UserPost) => {
     <NavigationContainer>
       <Drawer.Navigator>
         <Drawer.Screen name="test">
-          {() => <PostWithSingleReplyView post={post} reply={reply} />}
+          {() => (
+            <PostWithSingleReplyView
+              post={post}
+              reply={reply}
+              fullRepliesView={(post: UserPost) => (
+                <Text testID="fullReplies">{post.description}</Text>
+              )}
+            />
+          )}
         </Drawer.Screen>
       </Drawer.Navigator>
     </NavigationContainer>
   );
 };
 
-const navigateToCommentsThread = () => {
+const navigateToFullReplies = () => {
   fireEvent.press(screen.getByText("View All"));
 };
 
-const expectCommentsThreadToNotBeLoaded = () => {
-  expect(screen.queryByText("Replying to")).toBeNull();
+const expectFullRepliesThreadToNotBeLoaded = () => {
+  expect(screen.queryByTestId("fullReplies")).toBeNull();
 };
 
-const expectCommentsThreadDidLoad = async () => {
-  expect(await screen.findByText("Replying to")).toBeDefined();
+const expectFullRepliesThreadDidLoad = async () => {
+  expect(await screen.findByTestId("fullReplies")).toBeDefined();
 };
