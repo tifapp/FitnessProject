8cba984b379de3155a03774088829d752a2c9afa
ZoroarkZark <ilikepie1464@gmail.com>
2023-02-08 09:56:30 -0800

    Adding to work in storybook

---
+++ .storybook/components/MapComponent/MapTestData.tsx
@@ -0,0 +1,16 @@
+const state = {
+    markers: [
+      {
+        key: 1,
+        place: "Place one",
+        lat: 34.079761,
+        lng: -118.296802
+      },
+      {
+        key: 2,
+        place: "Place two",
+        lat: 34.039761,
+        lng: -118.256802
+      }
+    ]
+  }
