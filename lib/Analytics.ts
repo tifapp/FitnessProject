cda888144b4c464f9be44cff643d09e6f351d4d1
Matthew Hayes <matthewha853@gmail.com>
2023-02-18 13:51:16 -0800

    Cleanup

--- lib/Analytics.ts
+++ lib/Analytics.ts
@@ -6,27 +6,25 @@ export const amplifyRecordAnalytics = (
 ) => {
   Analytics.record({
     name: eventName,
-    attributes: compactMap(data ?? {}, ([_, value]) => {
+    attributes: compactMapAnaylticsData(data ?? {}, (value) => {
       if (typeof value === "string") return value
       return null
     }),
-    metrics: compactMap(data ?? {}, ([_, value]) => {
+    metrics: compactMapAnaylticsData(data ?? {}, (value) => {
       if (typeof value === "number") return value
       return null
     })
   })
 }
 
-const compactMap = <T extends Record<string, string | number>, V>(
+const compactMapAnaylticsData = <T extends Record<string, string | number>, V>(
   object: T,
-  mapper: (pair: [string, string | number]) => V | null
+  mapper: (value: string | number) => V | null
 ) => {
   const map = new Map<string, V>()
-
-  for (const [key, value] of Object.entries(object)) {
-    const mapped = mapper([key, value])
+  Object.entries(object).forEach(([key, value]) => {
+    const mapped = mapper(value)
     if (mapped) map.set(key, mapped)
-  }
-
+  })
   return Object.fromEntries(map)
 }
