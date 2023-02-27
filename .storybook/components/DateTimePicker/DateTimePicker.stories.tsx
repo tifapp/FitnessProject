f6cca12961df0432d7f385b6662a16542b85bcd4
Matthew Hayes <matthewha853@gmail.com>
2023-02-26 17:30:35 -0800

    Update DateTimePicker story

--- .storybook/components/DateTimePicker/DateTimePicker.stories.tsx
+++ .storybook/components/DateTimePicker/DateTimePicker.stories.tsx
@@ -1,12 +1,26 @@
 import { ComponentMeta, ComponentStory } from "@storybook/react-native"
 import { useState } from "react"
+import { View, Text } from "react-native"
 import DateTimePicker, {
   DateTimePickerProps
 } from "../../../components/formComponents/DateTimePicker"
 
 const Picker = (props: DateTimePickerProps) => {
   const [date, setDate] = useState(new Date())
-  return <DateTimePicker {...props} date={date} onDateChanged={setDate} />
+  return (
+    <View>
+      <DateTimePicker
+        {...props}
+        date={date}
+        labelStyle={{ fontWeight: "bold" }}
+        onDateChanged={setDate}
+      />
+      <Text>React state representation of selected date:</Text>
+      <Text>
+        {date.toDateString()} {date.toTimeString()}
+      </Text>
+    </View>
+  )
 }
 
 const DateTimePickerMeta: ComponentMeta<typeof DateTimePicker> = {
