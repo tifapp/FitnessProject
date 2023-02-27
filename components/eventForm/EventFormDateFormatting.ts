23a64e0cab0f05d8949d1b0e0b6373764776101b
Matthew Hayes <matthewha853@gmail.com>
2023-02-26 23:54:50 -0800

    Cleanup

--- components/eventForm/EventFormDateFormatting.ts
+++ components/eventForm/EventFormDateFormatting.ts
@@ -2,28 +2,28 @@ import { FixedDateRange } from "@lib/Date"
 import { dayjs } from "@lib/dayjs"
 import { Dayjs } from "dayjs"
 
+/**
+ * Formats a date range as it appears in the `EventFormToolbar` component.
+ */
 export const eventFormFormatDateRange = (dateRange: FixedDateRange) => {
   const start = dayjs(dateRange.startDate)
   const end = dayjs(dateRange.endDate)
 
-  const startsAndEndsOnSameDay = end.isSame(start, "day")
-
-  const startTimeFormatted = formatTime(start)
-  const endTimeFormatted = formatTime(end)
-
-  const endDateFormat = startsAndEndsOnSameDay
-    ? endTimeFormatted
-    : `${end.isTomorrow() ? "Tomorrow" : formatDate(end)} ${endTimeFormatted}`
+  const endDateFormat = end.isSame(start, "day")
+    ? formatTime(end)
+    : formatDateTime(end)
 
   const startDateFormat = start.isToday()
-    ? `Today ${startTimeFormatted}`
-    : `${
-      start.isTomorrow() ? "Tomorrow" : formatDate(start)
-    } ${startTimeFormatted}`
+    ? `Today ${formatTime(start)}`
+    : formatDateTime(start)
+
   return `${startDateFormat} - ${endDateFormat}`
 }
 
-const formatDate = (date: Dayjs) => date.format("MMM D,")
+const formatDateTime = (date: Dayjs) => {
+  const formattedDate = date.isTomorrow() ? "Tomorrow" : date.format("MMM D,")
+  return `${formattedDate} ${formatTime(date)}`
+}
 
 const formatTime = (date: Dayjs) => {
   return date.format(date.minute() !== 0 ? "h:mma" : "ha")
