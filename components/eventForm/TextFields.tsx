722d18b8e90d0c09811b83074d2be1c8dbf37c7c
Matthew Hayes <matthewha853@gmail.com>
2023-03-01 23:12:23 -0800

    Prevent title > 75 characters from being entered

--- components/eventForm/TextFields.tsx
+++ components/eventForm/TextFields.tsx
@@ -7,8 +7,19 @@ import { useEventFormField } from "."
  */
 export const EventFormTitleField = () => {
   const [title, setTitle] = useEventFormField("title")
-  return <TextInput placeholder="Title" value={title} onChangeText={setTitle} />
+  return (
+    <TextInput
+      placeholder="Title"
+      maxLength={MAX_TITLE_LENGTH}
+      value={title}
+      onChangeText={(text) => {
+        setTitle(text.substring(0, Math.min(text.length, MAX_TITLE_LENGTH)))
+      }}
+    />
+  )
 }
+
+const MAX_TITLE_LENGTH = 75
 
 /**
  * The description field for an event form.
---
+++ tests/events/EventFormComponents/TitleField.test.tsx
@@ -0,0 +1,39 @@
+import {
+  EventForm,
+  EventFormTitleField,
+  useEventFormContext
+} from "@components/eventForm"
+import { render, screen } from "@testing-library/react-native"
+import React from "react"
+import { View } from "react-native"
+import { baseTestEventFormValues, editEventTitle } from "./helpers"
+import "../../helpers/Matchers"
+
+describe("EventFormTitleField tests", () => {
+  it("cannot hold a title longer than 75 characters", () => {
+    renderTitleField()
+    editEventTitle("a".repeat(76))
+    expect(currentTitle("a".repeat(75))).toBeDisplayed()
+  })
+})
+
+const renderTitleField = () => {
+  return render(
+    <EventForm
+      initialValues={baseTestEventFormValues}
+      onSubmit={jest.fn()}
+      onDismiss={jest.fn()}
+    >
+      <CurrentTitle />
+      <EventFormTitleField />
+    </EventForm>
+  )
+}
+
+const CurrentTitle = () => {
+  return <View testID={useEventFormContext().watch("title")} />
+}
+
+const currentTitle = (title: string) => {
+  return screen.queryByTestId(title)
+}
