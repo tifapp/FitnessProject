08ae5d3a223617f4aa181bc3dfff6ec0d989bf3a
Matthew Hayes <matthewha853@gmail.com>
2023-03-11 14:37:53 -0800

    Handles normal amplify geo interactions

--- lib/location/search/Client.ts
+++ lib/location/search/Client.ts
@@ -1,3 +1,4 @@
+import { Geo } from "aws-amplify"
 import { Location } from "../Location"
 
 /**
@@ -8,3 +9,37 @@ export type LocationSearchResult = Readonly<{
   formattedAddress?: string
   coordinates: Location
 }>
+
+export type LocationTextSearchOptions = {
+  biasLocation: Location
+  limit: number
+}
+
+export interface LocationSearchClient {
+  textSearch: (
+    query: string,
+    options?: LocationTextSearchOptions
+  ) => Promise<LocationSearchResult[]>
+}
+
+export class AmplifyLocationSearchClient implements LocationSearchClient {
+  async textSearch (query: string, options?: LocationTextSearchOptions) {
+    return await Geo.searchByText(query, {
+      countries: ["USA"],
+      biasPosition: options
+        ? [options.biasLocation.longitude, options.biasLocation.latitude]
+        : undefined,
+      maxResults: options?.limit
+    }).then((results) =>
+      results.map((result) => {
+        const [longitude, latitude] = result.geometry!!.point
+        const addressMatches = result.label?.match(/(.+?), (.*)/)
+        return {
+          name: addressMatches?.[1],
+          formattedAddress: addressMatches?.[2],
+          coordinates: { latitude, longitude }
+        }
+      })
+    )
+  }
+}
---
+++ tests/location/search/Client.test.ts
@@ -0,0 +1,69 @@
+import {
+  AmplifyLocationSearchClient,
+  LocationSearchClient
+} from "@lib/location/search"
+import { Geo } from "aws-amplify"
+import { Place } from "aws-sdk/clients/location"
+
+jest.mock("aws-amplify")
+const amplifyTextSearchSpy = jest.spyOn(Geo, "searchByText")
+
+const testTextQuery = "McDonalds Play Places"
+
+let client: LocationSearchClient
+describe("AmplifyLocationSearchClient tests", () => {
+  beforeEach(() => {
+    client = new AmplifyLocationSearchClient()
+    amplifyTextSearchSpy.mockReset()
+  })
+
+  it("creates a proper request to amplify geo with custom options", async () => {
+    amplifyTextSearchSpy.mockResolvedValue([])
+    await client.textSearch(testTextQuery, {
+      biasLocation: { latitude: 42, longitude: -121 },
+      limit: 25
+    })
+    expect(amplifyTextSearchSpy).toHaveBeenCalledWith(testTextQuery, {
+      countries: ["USA"],
+      maxResults: 25,
+      biasPosition: [-121, 42]
+    })
+  })
+
+  it("creates a proper request to amplify geo with no custom options", async () => {
+    amplifyTextSearchSpy.mockResolvedValue([])
+    await client.textSearch(testTextQuery)
+    expect(amplifyTextSearchSpy).toHaveBeenCalledWith(testTextQuery, {
+      countries: ["USA"]
+    })
+  })
+
+  it("handlesa normal amplify geo text search response properly", async () => {
+    amplifyTextSearchSpy.mockResolvedValue([
+      {
+        geometry: {
+          point: [-122.36738, 47.61609]
+        },
+        label: "Amazon Go, 2131 7th Ave, Seattle, WA, 98121, USA"
+      },
+      {
+        geometry: {
+          point: [-112.10668, 46.31909]
+        },
+        label: "Amazon, Jefferson City, MT, United States"
+      }
+    ])
+    expect(await client.textSearch(testTextQuery)).toEqual([
+      {
+        name: "Amazon Go",
+        formattedAddress: "2131 7th Ave, Seattle, WA, 98121, USA",
+        coordinates: { latitude: 47.61609, longitude: -122.36738 }
+      },
+      {
+        name: "Amazon",
+        formattedAddress: "Jefferson City, MT, United States",
+        coordinates: { latitude: 46.31909, longitude: -112.10668 }
+      }
+    ])
+  })
+})
