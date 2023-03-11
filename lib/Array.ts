7b7e7e2d9ec230cc5f272f38fd6d7d8fb39a4b7b
Matthew Hayes <matthewha853@gmail.com>
2023-03-10 16:56:25 -0800

    Fix type inference on ArrayUtils.removeOptionals

--- lib/Array.ts
+++ lib/Array.ts
@@ -8,7 +8,7 @@ export namespace ArrayUtils {
    * A typesafe way for removing all null/undefined values from an array.
    */
   export const removeOptionals = <T>(arr: (T | null | undefined)[]) => {
-    return arr.reduce((acc, curr) => (curr ? [...acc, curr] : acc), [] as T[])
+    return arr.reduce((acc, curr) => (curr ? [...acc, curr] : acc), [])
   }
 
   /**
