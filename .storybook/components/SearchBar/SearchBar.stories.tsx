82c0fb0a8d2a915130506cf8b3d5c5b53c678a2f
Matthew Hayes <matthewha853@gmail.com>
2023-03-19 00:02:25 -0700

    Search bar styling

---
+++ .storybook/components/SearchBar/SearchBar.stories.tsx
@@ -0,0 +1,28 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react-native"
+import { SearchBar } from "@components/SearchBar"
+import { useState } from "react"
+import { MaterialIcon } from "@components/common/Icons"
+
+const PreviewSearchBar = () => {
+  const [text, setText] = useState("")
+  return (
+    <SearchBar
+      style={{ paddingHorizontal: 16, paddingVertical: 8 }}
+      leftAddon={<MaterialIcon name="search" />}
+      placeholder={"Search Something..."}
+      text={text}
+      onTextChanged={setText}
+    />
+  )
+}
+
+const SearchBarMeta: ComponentMeta<typeof PreviewSearchBar> = {
+  title: "SearchBar",
+  component: PreviewSearchBar
+}
+
+export default SearchBarMeta
+
+type SearchBarStory = ComponentStory<typeof PreviewSearchBar>
+
+export const Default: SearchBarStory = () => <PreviewSearchBar />
--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,6 +1,12 @@
 import React, { ReactNode } from "react"
-import { StyleProp, TextInput, View, ViewStyle } from "react-native"
-import { TouchableOpacity } from "react-native-gesture-handler"
+import {
+  StyleProp,
+  TextInput,
+  View,
+  ViewStyle,
+  TouchableOpacity,
+  StyleSheet
+} from "react-native"
 import { MaterialCommunityIcon } from "./common/Icons"
 
 export type SearchBarProps = {
@@ -11,6 +17,9 @@ export type SearchBarProps = {
   style?: StyleProp<ViewStyle>
 }
 
+/**
+ * A generic search bar component.
+ */
 export const SearchBar = ({
   leftAddon,
   text,
@@ -19,19 +28,70 @@ export const SearchBar = ({
   style
 }: SearchBarProps) => (
   <View style={style}>
-    {leftAddon}
-    <TextInput
-      placeholder={placeholder}
-      value={text}
-      onChangeText={onTextChanged}
-    />
-    {text.length > 0 && (
-      <TouchableOpacity
-        onPress={() => onTextChanged("")}
-        accessibilityLabel="Clear all search text"
-      >
-        <MaterialCommunityIcon name="close-circle-outline" size={24} />
-      </TouchableOpacity>
-    )}
+    <View style={styles.card}>
+      <View style={styles.container}>
+        <View style={styles.leftAddon}>{leftAddon}</View>
+        <View style={styles.leftContainer}>
+          <TextInput
+            placeholder={placeholder}
+            value={text}
+            onChangeText={onTextChanged}
+            style={styles.textInput}
+          />
+        </View>
+        {text.length > 0 && (
+          <TouchableOpacity
+            onPress={() => onTextChanged("")}
+            accessibilityLabel="Clear all search text"
+            style={styles.clearIconContainer}
+            hitSlop={{ top: 12, left: 12, bottom: 12, right: 12 }}
+          >
+            <MaterialCommunityIcon
+              style={styles.clearIcon}
+              name="close"
+              size={21}
+            />
+          </TouchableOpacity>
+        )}
+      </View>
+    </View>
   </View>
 )
+
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: "white",
+    borderWidth: 1,
+    borderRadius: 12,
+    borderColor: "rgba(0, 0, 0, 0.05)"
+  },
+  container: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    width: "100%",
+    padding: 8
+  },
+  leftContainer: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    flex: 1
+  },
+  leftAddon: {
+    marginRight: 8
+  },
+  textInput: {
+    fontSize: 21
+  },
+  clearIconContainer: {
+    marginLeft: 16,
+    backgroundColor: "rgba(0, 0, 0, 0.05)",
+    borderRadius: 12
+  },
+  clearIcon: {
+    color: "black",
+    padding: 4,
+    opacity: 0.35
+  }
+})
