13887e93618cd9020ccedc3eb7f146e35ec77803
Matthew Hayes <matthewha853@gmail.com>
2023-04-05 00:10:46 -0700

    Add documentation

--- hooks/UserLocationHooks.ts
+++ hooks/UserLocationHooks.ts
@@ -7,19 +7,24 @@ import { useDependencyValue } from "@lib/dependencies"
 import { useEffect, useState } from "react"
 
 /**
- * A hook to observe the user's current location.
+ * A hook to observe the user's current location using the
+ * `UserLocationDependencyKeys.track` dependency.
  *
- * This hook uses the {@link userLocationDependencyKey} to perform
- * the tracking.
- *
- * @param options see {@link UserLocationTrackingOptions}
- * @returns
+ * @param accurracy The accurracy at which to track the user's location.
+ * Defaults to `low-accurracy`.
  */
-export const useUserLocation = (accurracy: UserLocationTrackingAccurracy = "low-accurracy") => {
+export const useUserLocation = (
+  accurracy: UserLocationTrackingAccurracy = "low-accurracy"
+) => {
   const trackLocation = useDependencyValue(UserLocationDependencyKeys.track)
-  const [location, setLocation] = useState<UserLocationTrackingUpdate>({ status: "undetermined" })
+  const [location, setLocation] = useState<UserLocationTrackingUpdate>({
+    status: "undetermined"
+  })
 
-  useEffect(() => trackLocation(accurracy, setLocation), [trackLocation, accurracy])
+  useEffect(
+    () => trackLocation(accurracy, setLocation),
+    [trackLocation, accurracy]
+  )
 
   return location
 }
--- lib/location/UserLocation.ts
+++ lib/location/UserLocation.ts
@@ -1,33 +1,53 @@
 import { TrackedLocation } from "./Location"
-import {
-  watchPositionAsync,
-  requestForegroundPermissionsAsync,
-  getLastKnownPositionAsync,
-  getCurrentPositionAsync,
-  LocationObject,
-  LocationAccuracy
-} from "expo-location"
-import { createDependencyKey, useDependencyValue } from "../dependencies"
-import { useEffect, useRef, useState } from "react"
+import { createDependencyKey } from "../dependencies"
 
 // Android -> E_LOCATION_UNAUTHORIZED
 // iOS (Perms Denied) -> E_NO_PERMISSIONS
 // iOS (Services Disabled) -> E_LOCATION_SERVICES_DISABLED
+
+/**
+ * An accurracy to track the user's location.
+ *
+ * A higher accurracy may consume more device resources.
+ *
+ * `low-accurracy`: Accurrate within 1km.
+ * `precise-accurracy`: The most precise accurracy possible.
+ */
 export type UserLocationTrackingAccurracy = "low-accurracy" | "precise-accurracy"
 
-export type UserLocationTrackingError = "no-permissions" | "services-disabled"
+/**
+ * An error that can occur when tracking a user's location.
+ */
+export type UserLocationTrackingError = "no-permissions" | "services-disabled" | "location-unavailable"
 
+/**
+ * An update published by to a (@link TrackUserLocation} callback.
+ */
 export type UserLocationTrackingUpdate =
   { status: "error", error: UserLocationTrackingError }
   | { status: "undetermined" }
   | { status: "success", location: TrackedLocation }
 
 export type StopUserLocationTracking = () => void
+
+/**
+ * A function type to track a user's location.
+ *
+ * @param accurracy The accurracy of which to track the user's location.
+ * @param callback Handles an {@link UserLocationTrackingUpdate}.
+ * @returns A function to stop tracking the user's location.
+ */
 export type TrackUserLocation = (
   accuracy: UserLocationTrackingAccurracy,
   callback: (update: UserLocationTrackingUpdate) => void
 ) => StopUserLocationTracking
 
+/**
+ * Dependency Keys relating to operations around the user's location.
+ */
 export namespace UserLocationDependencyKeys {
-  export const track = createDependencyKey<TrackUserLocation>(() => () => { })
+  /**
+   * A dependency key to track the user's location.
+   */
+  export const track = createDependencyKey<TrackUserLocation>(() => () => { /* TODO */ })
 }
