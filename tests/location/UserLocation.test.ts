5acb03eb8d31b1c0148a384bb5244918fdadc8ab
Matthew Hayes <matthewha853@gmail.com>
2023-04-08 22:37:29 -0700

    Cleanup Tests

--- tests/location/UserLocation.test.ts
+++ tests/location/UserLocation.test.ts
@@ -4,16 +4,29 @@ import {
   LocationCoordinatesMocks,
   UserLocationTrackingAccurracy
 } from "@lib/location"
+import { waitFor } from "@testing-library/react-native"
 
 const testAccuracy = "precise"
 
 describe("UserLocation tests", () => {
   describe("ExpoUserLocationTracking tests", () => {
     test("translates accurracy to proper expo accurracy when making tracking request", () => {
-      expectAccuracyConversion("approximate-low", LocationAccuracy.Low)
-      expectAccuracyConversion("approximate-medium", LocationAccuracy.Balanced)
-      expectAccuracyConversion("precise", LocationAccuracy.Highest)
+      expectMakesExpoRequestWithAccurracy("approximate-low", LocationAccuracy.Low)
+      expectMakesExpoRequestWithAccurracy("approximate-medium", LocationAccuracy.Balanced)
+      expectMakesExpoRequestWithAccurracy("precise", LocationAccuracy.Highest)
     })
+
+    const expectMakesExpoRequestWithAccurracy = (
+      accuracy: UserLocationTrackingAccurracy,
+      expoAccuracy: LocationAccuracy
+    ) => {
+      const track = jest.fn().mockResolvedValue(jest.fn())
+      expoTrackUserLocation(accuracy, jest.fn(), track)
+      expect(track).toHaveBeenCalledWith(
+        expect.objectContaining({ accuracy: expoAccuracy }),
+        expect.any(Function)
+      )
+    }
 
     test("translates expo location update into TrackedLocation", () => {
       const callback = jest.fn()
@@ -37,54 +50,23 @@ describe("UserLocation tests", () => {
 
     it("sends an error update when expo tracker fails", async () => {
       const callback = jest.fn()
-      const track = jest.fn()
-
-      // NB: Expo makes their API asynchronous, however since we're in
-      // a syncronous context we use a promise to ensure that our callback
-      // actually consumes the error event before the test ends.
-      const errorPromise = new Promise<void>((resolve) => {
-        track.mockImplementation(() => {
-          resolve()
-          return Promise.reject(new Error())
-        })
-      })
+      const track = jest.fn().mockRejectedValue(new Error())
 
       expoTrackUserLocation(testAccuracy, callback, track)
-      await errorPromise
-      expect(callback).toHaveBeenCalledWith({ status: "error" })
+      await waitFor(() => {
+        expect(callback).toHaveBeenCalledWith({ status: "error" })
+      })
     })
 
     it("unsubscribes from expo tracking when unsub invoked", async () => {
-      const track = jest.fn()
       const unsubFromExpo = jest.fn()
-
-      // NB: The unsub function is in a "fire-and-forget" style, which means
-      // we can't tell when it will actually unsubscribe from expo (since
-      // expo's tracker returns a promise). This promise ensures that we give
-      // a chance for the tracker to resolve before asserting.
-      const trackPromise = new Promise<void>((resolve) => {
-        track.mockImplementation(() => {
-          resolve()
-          return Promise.resolve({ remove: unsubFromExpo })
-        })
-      })
+      const track = jest.fn().mockResolvedValue({ remove: unsubFromExpo })
 
       const unsub = expoTrackUserLocation(testAccuracy, jest.fn(), track)
       unsub()
-      await trackPromise
-      expect(unsubFromExpo).toHaveBeenCalled()
+      await waitFor(() => {
+        expect(unsubFromExpo).toHaveBeenCalled()
+      })
     })
-
-    const expectAccuracyConversion = (
-      accuracy: UserLocationTrackingAccurracy,
-      expoAccuracy: LocationAccuracy
-    ) => {
-      const track = jest.fn().mockResolvedValue(jest.fn())
-      expoTrackUserLocation(accuracy, jest.fn(), track)
-      expect(track).toHaveBeenCalledWith(
-        expect.objectContaining({ accuracy: expoAccuracy }),
-        expect.any(Function)
-      )
-    }
   })
 })
