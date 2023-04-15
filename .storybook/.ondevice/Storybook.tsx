428d82da7a0cabf1b50247092a8b869665f58410
tifapp <tifapppublic@gmail.com>
2023-04-15 00:33:18 -0700

    Create custom ui for storybook

--- .storybook/.ondevice/Storybook.tsx
+++ .storybook/.ondevice/Storybook.tsx
@@ -1,7 +1,64 @@
-import { getStorybookUI } from "@storybook/react-native"
-import "./storybook.requires"
+import React, { useState } from "react"
+import { Text, TouchableOpacity, View } from "react-native"
 
-const StorybookUIRoot = getStorybookUI({
-  shouldDisableKeyboardAvoidingView: true
-})
-export default StorybookUIRoot
+// Import your stories
+import { MyButtonProps } from ".storybook/components/Button/Button"
+import { FlatList } from "react-native-gesture-handler"
+import MyButtonMeta, {
+  Basic as MyButtonBasic
+} from "../components/Button/Button.stories"
+
+// Create an array of stories
+const stories = [
+  {
+    name: MyButtonMeta.title,
+    component: MyButtonBasic,
+    args: MyButtonMeta.args as MyButtonProps
+  }
+  // Add more stories here...
+]
+
+const CustomStorybookUI = () => {
+  const [selectedStory, setSelectedStory] = useState(-1)
+
+  // Render the selected story
+  if (selectedStory !== -1) {
+    const { component: StoryComponent, args } = stories[selectedStory]
+    return (
+      <>
+        <StoryComponent {...args} />
+        <Text
+          onPress={() => setSelectedStory(-1)}
+          style={{ position: "absolute", bottom: 10, left: 10 }}
+        >
+          Close
+        </Text>
+      </>
+    )
+  }
+
+  // Render the story list
+  return (
+    <View style={{ flex: 1, margin: 20 }}>
+      <FlatList
+        data={stories}
+        renderItem={({ item, index }) => (
+          <TouchableOpacity
+            style={{
+              padding: 10,
+              width: "100%",
+              borderWidth: 1,
+              borderColor: "gray"
+            }}
+            key={index}
+            onPress={() => setSelectedStory(index)}
+          >
+            <Text>{item.name}</Text>
+          </TouchableOpacity>
+        )}
+      />
+    </View>
+  )
+}
+
+export default CustomStorybookUI
