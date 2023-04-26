a4510af93804c48207f78750a29e10b5de030ab7
ZoroarkZark <ilikepie1464@gmail.com>
2023-04-25 18:15:20 -0700

    Developing attendeeEntryScreen

--- screens/AttendeesListScreen.tsx
+++ screens/AttendeesListScreen.tsx
@@ -1,44 +1,52 @@
-import IconButton from "@components/common/IconButton";
-import React from "react";
-import { Button, FlatList, View } from "react-native";
+import { AttendeeEntry } from "@components/attendeesList/attendeeEntry"
+import IconButton from "@components/common/IconButton"
+import { EventAttendee } from "@lib/events"
+import React from "react"
+import { FlatList, View } from "react-native"
+import { Divider } from "react-native-elements"
 
-const someData = [
-    {
-        id: '1',
-        imageURL: "Japan.com",
-        name: "Cool"
-    },
-    {
-        id: '2',
-        imageURL: "USA.com",
-        name: "Also cool"
-    }
-];
+const someData: EventAttendee[] = [
+  {
+    id: "1",
+    username: "Cool"
+  },
+  {
+    id: "2",
+    username: "Also cool"
+  }
+]
 
-
-function goBack() {
-    //Presumably do navigate back to the previous screen you were on
-    return;
+function goBack () {
+  // Presumably use navigate back to the previous screen you were on
 }
 
+// Take in a eventAttendee[] as a prop
+
 export const AttendeesListScreen = () => {
-    //List of attendees
+  // List of attendees
 
-    //Button that would let you go back, goes on the top left
-    //Could also use setOptions in navigation itself, for headerLeft
-        
+  // Button that would let you go back, goes on the top left
+  // Could also use setOptions in navigation itself, for headerLeft
+  const FlatSeparator = () => {
+    return (
+      <View
+        style={{
+          height: 1,
+          width: "100%",
+          backgroundColor: "#000"
+        }}
+      />
+    )
+  }
+
+  return (
     <View>
-        <IconButton iconName="height" onPress={goBack}/>
-        <FlatList 
-            data={someData}
-            renderItem={({item}) => <Button title="cool"/>}
-            keyExtractor={item => item.thing}
-        />
+      <IconButton iconName="height" onPress={goBack} />
+      <FlatList
+        ItemSeparatorComponent={FlatSeparator}
+        data={someData}
+        renderItem={({ item }) => <AttendeeEntry attendee={item} />}
+      />
     </View>
-    }
-            
-
-
-
-
+  )
 }
