45ec8fb7fc3470f80ed78402f5dce6126180b939
nicoletteL <nantisde@ucsc.edu>
2023-04-25 20:09:12 -0700

    Added button at bottom

--- screens/EventDetails/EventDetails.tsx
+++ screens/EventDetails/EventDetails.tsx
@@ -1,14 +1,7 @@
-import { CurrentUserEvent } from "@lib/events"
+import { CurrentUserEvent, isAttendingEvent } from "@lib/events"
 import React from "react"
 import { Ionicons } from "@expo/vector-icons"
-import {
-  Image,
-  ScrollView,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View
-} from "react-native"
+import { Image, ScrollView, StyleSheet, View } from "react-native"
 import { Caption, Headline, Title } from "@components/Text"
 import ExpandableText from "@screens/EventDetails/ExpandableText"
 import { Divider } from "react-native-elements"
@@ -17,111 +10,128 @@ import TimeSection from "./TimeSection"
 import LocationSection from "./LocationSection"
 import AttendeeSection from "./AttendeeSection"
 import ChatSection from "./ChatSection"
+import { OutlinedButton, PrimaryButton } from "@components/common/Buttons"
 
 export type EventDetailsProps = {
   event: CurrentUserEvent
 }
 
 const MARKER_SIZE = 44
+const BOTTOM_TAB_HEIGHT = 80
 
 const EventDetails = ({ event }: EventDetailsProps) => {
   const isFriend = false
 
   return (
-    <ScrollView
-      style={[styles.container, styles.spacing]}
-      contentContainerStyle={{ flexGrow: 1 }}
-      nestedScrollEnabled={true}
-    >
-      <Title>{event.title}</Title>
-      <View style={[styles.flexRow, { marginVertical: 16 }]}>
-        <Image
-          style={[styles.image]}
-          source={require("../../assets/icon.png")}
-        />
-        <View style={{ flex: 1 }}>
-          <View style={styles.flexRow}>
-            <Headline>{event.host.username}</Headline>
-            {!isFriend && (
-              <View style={[styles.flexRow, { alignItems: "center" }]}>
-                <Ionicons
-                  style={{ marginHorizontal: 4 }}
-                  size={6}
-                  name="ellipse"
-                />
-                <Headline style={{ color: event.color }}>Add Friend</Headline>
-              </View>
-            )}
-          </View>
-          <Caption>{event.host.handle}</Caption>
-        </View>
-      </View>
-
-      <View style={styles.iconSection}>
-        <TimeSection color={event.color} duration={event.dateRange} />
-
-        <Divider style={styles.divider} />
-
-        <LocationSection
-          color={event.color}
-          coordinates={event.coordinates}
-          placemark={event.placemark}
-        />
-
-        <Divider style={styles.divider} />
-
-        <AttendeeSection
-          color={event.color}
-          attendeeCount={event.attendeeCount}
-        />
-
-        <Divider style={styles.divider} />
-
-        <ChatSection
-          color={event.color}
-          userAttendeeStatus={event.userAttendeeStatus}
-        />
-      </View>
-
-      {event.description && (
-        <View style={{ marginTop: 16 }}>
-          <Headline style={{ marginBottom: 4 }}>About</Headline>
-          <ExpandableText
-            props={{ style: { color: event.color, marginTop: 5 } }}
-            text={event.description}
-            linesToDisplay={3}
-          />
-        </View>
-      )}
-      <Headline style={{ marginTop: 16, marginBottom: 8 }}>Location</Headline>
-      <EventMapSnippet
-        style={{ width: "100%", height: 160, marginBottom: 16 }}
-        minZoomLevel={12}
-        initialRegion={{
-          latitude: event.coordinates.latitude,
-          longitude: event.coordinates.longitude,
-          latitudeDelta: 0.1,
-          longitudeDelta: 0.1
-        }}
-        renderMarker={() => (
+    <View>
+      <ScrollView
+        style={[styles.container, styles.spacing]}
+        contentContainerStyle={{ flexGrow: 1 }}
+        nestedScrollEnabled={true}
+      >
+        <Title>{event.title}</Title>
+        <View style={[styles.flexRow, { marginVertical: 16 }]}>
           <Image
+            style={[styles.image]}
             source={require("../../assets/icon.png")}
-            style={{
-              width: MARKER_SIZE,
-              height: MARKER_SIZE,
-              borderWidth: 1,
-              borderColor: "white",
-              borderRadius: 20
-            }}
           />
-        )}
-        marker={{ key: event.id, location: event.coordinates }}
-      />
+          <View style={{ flex: 1 }}>
+            <View style={styles.flexRow}>
+              <Headline>{event.host.username}</Headline>
+              {!isFriend && (
+                <View style={[styles.flexRow, { alignItems: "center" }]}>
+                  <Ionicons
+                    style={{ marginHorizontal: 4 }}
+                    size={6}
+                    name="ellipse"
+                  />
+                  <Headline style={{ color: event.color }}>Add Friend</Headline>
+                </View>
+              )}
+            </View>
+            <Caption>{event.host.handle}</Caption>
+          </View>
+        </View>
 
-      <Text>{event.description}</Text>
-      <Text>{event.description}</Text>
-      <Text>{event.description}</Text>
-    </ScrollView>
+        <View style={styles.iconSection}>
+          <TimeSection color={event.color} duration={event.dateRange} />
+
+          <Divider style={styles.divider} />
+
+          <LocationSection
+            color={event.color}
+            coordinates={event.coordinates}
+            placemark={event.placemark}
+          />
+
+          <Divider style={styles.divider} />
+
+          <AttendeeSection
+            color={event.color}
+            attendeeCount={event.attendeeCount}
+          />
+
+          <Divider style={styles.divider} />
+
+          <ChatSection
+            color={event.color}
+            userAttendeeStatus={event.userAttendeeStatus}
+          />
+        </View>
+
+        {event.description && (
+          <View style={{ marginTop: 16 }}>
+            <Headline style={{ marginBottom: 4 }}>About</Headline>
+            <ExpandableText
+              props={{ style: { color: event.color, marginTop: 5 } }}
+              text={event.description}
+              linesToDisplay={3}
+            />
+          </View>
+        )}
+        <View style={{ marginTop: 16, marginBottom: BOTTOM_TAB_HEIGHT + 24 }}>
+          <Headline style={{ marginBottom: 8 }}>Location</Headline>
+          <EventMapSnippet
+            style={{ width: "100%", height: 160 }}
+            minZoomLevel={12}
+            initialRegion={{
+              latitude: event.coordinates.latitude,
+              longitude: event.coordinates.longitude,
+              latitudeDelta: 0.1,
+              longitudeDelta: 0.1
+            }}
+            renderMarker={() => (
+              <Image
+                source={require("../../assets/icon.png")}
+                style={{
+                  width: MARKER_SIZE,
+                  height: MARKER_SIZE,
+                  borderWidth: 1,
+                  borderColor: "white",
+                  borderRadius: 20
+                }}
+              />
+            )}
+            marker={{ key: event.id, location: event.coordinates }}
+          />
+        </View>
+      </ScrollView>
+      <View style={styles.bottomTab}>
+        {isAttendingEvent(event.userAttendeeStatus)
+          ? (
+            <OutlinedButton
+              title={"Leave Event"}
+              buttonProps={{ style: styles.buttonStyle }}
+            />
+          )
+          : (
+            <PrimaryButton
+              title={"Join Now"}
+              buttonProps={{ style: styles.buttonStyle }}
+            />
+          )}
+      </View>
+    </View>
   )
 }
 
@@ -156,8 +166,17 @@ const styles = StyleSheet.create({
     alignSelf: "flex-end"
   },
   bottomTab: {
-    // position: "absolute",
-    backgroundColor: "yellow",
-    bottom: 0
+    flexDirection: "row",
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    right: 0,
+    height: BOTTOM_TAB_HEIGHT,
+    backgroundColor: "white"
+  },
+  buttonStyle: {
+    flex: 1,
+    marginHorizontal: 16
+    // marginBottom: 8
   }
 })
