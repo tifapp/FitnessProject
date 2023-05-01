d26cc19d92db925cdda23f13ee72ad0f87f8bb03
Matthew Hayes <matthewha853@gmail.com>
2023-05-01 01:47:16 -0700

    Report success screen view

--- screens/Reporting/SuccessView.tsx
+++ screens/Reporting/SuccessView.tsx
@@ -1,5 +1,14 @@
+import { BodyText, Headline } from "@components/Text"
+import { PrimaryButton } from "@components/common/Buttons"
+import { Ionicon } from "@components/common/Icons"
 import React from "react"
-import { Button, StyleProp, View, ViewStyle } from "react-native"
+import {
+  SafeAreaView,
+  StyleProp,
+  StyleSheet,
+  View,
+  ViewStyle
+} from "react-native"
 
 export type ReportSuccessProps = {
   onDoneTapped: () => void
@@ -10,7 +19,48 @@ export const ReportSuccessView = ({
   onDoneTapped,
   style
 }: ReportSuccessProps) => (
-  <View style={style}>
-    <Button title="Done" onPress={onDoneTapped} />
-  </View>
+  <SafeAreaView style={[style, styles.container]}>
+    <View style={styles.centerContainer}>
+      <Ionicon name="thumbs-up" size={64} />
+      <Headline style={styles.title}>Thanks for letting us know</Headline>
+      <BodyText style={styles.bodyText}>
+        Your input helps us keep fitness communities safe.
+      </BodyText>
+    </View>
+    <View style={styles.doneButtonContainer}>
+      <PrimaryButton
+        title="Done"
+        buttonProps={{ onPress: onDoneTapped, style: styles.doneButton }}
+      />
+    </View>
+  </SafeAreaView>
 )
+
+const styles = StyleSheet.create({
+  container: {
+    display: "flex",
+    flex: 1
+  },
+  doneButtonContainer: {
+    paddingHorizontal: 16,
+    paddingBottom: 24
+  },
+  doneButton: {
+    width: "100%"
+  },
+  centerContainer: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    paddingHorizontal: 16
+  },
+  title: {
+    textAlign: "center",
+    marginVertical: 8
+  },
+  bodyText: {
+    textAlign: "center",
+    opacity: 0.5,
+    maxWidth: "75%"
+  }
+})
