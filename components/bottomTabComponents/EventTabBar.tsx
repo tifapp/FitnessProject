f69a65028747b2101d6be95ab227110b4cb78b50
nicoletteL <nantisde@ucsc.edu>
2023-05-03 18:17:18 -0700

    Styling changes

--- components/bottomTabComponents/EventTabBar.tsx
+++ components/bottomTabComponents/EventTabBar.tsx
@@ -1,38 +1,28 @@
 import { TouchableIonicon } from "@components/common/Icons"
-import { ButtonStyles } from "@lib/ButtonStyle"
-import { BlurView } from "expo-blur"
+import { AppStyles } from "@lib/AppColorStyle"
 import React from "react"
 import { StyleSheet, View } from "react-native"
 
 export const EventTabBar = () => {
-  const darkIconColor = "#1c1c1c"
-  const iconSize = 24
-
   return (
-    <BlurView intensity={100} tint="light" style={styles.blur}>
-      <View style={styles.container}>
-        <TouchableIonicon
-          style={styles.icon}
-          icon={{ name: "location", color: ButtonStyles.darkColor }}
-        />
-        <TouchableIonicon
-          style={styles.icon}
-          icon={{ name: "chatbox", color: ButtonStyles.darkColor }}
-        />
-        <TouchableIonicon
-          style={styles.plusIcon}
-          icon={{ name: "add-outline", color: "white" }}
-        />
-        <TouchableIonicon
-          style={styles.icon}
-          icon={{ name: "notifications", color: "grey" }}
-        />
-        <TouchableIonicon
-          style={styles.icon}
-          icon={{ name: "person", color: "grey" }}
-        />
-      </View>
-    </BlurView>
+    <View style={styles.container}>
+      <TouchableIonicon
+        icon={{ name: "map", color: AppStyles.darkColor }}
+      />
+      <TouchableIonicon
+        icon={{ name: "chatbox", color: AppStyles.colorOpacity35 }}
+      />
+      <TouchableIonicon
+        style={styles.plusIcon}
+        icon={{ name: "add-outline", color: "white" }}
+      />
+      <TouchableIonicon
+        icon={{ name: "notifications", color: AppStyles.colorOpacity35 }}
+      />
+      <TouchableIonicon
+        icon={{ name: "person", color: AppStyles.colorOpacity35 }}
+      />
+    </View>
   )
 }
 
@@ -41,32 +31,21 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     position: "absolute",
     justifyContent: "space-around",
+    alignItems: "center",
     bottom: 0,
     left: 0,
     right: 0,
     height: 80,
     backgroundColor: "white"
   },
-  blur: {
-    position: "absolute",
-    bottom: 0,
-    left: 0,
-    right: 0,
-    height: 90
-  },
-  icon: {
-    alignItems: "center",
-    justifyContent: "center"
-  },
   plusIcon: {
     alignItems: "center",
     justifyContent: "center",
-    top: "4%",
-    width: 50,
-    height: 50,
+    width: 48,
+    height: 48,
     padding: 4,
-    borderRadius: 15,
-    backgroundColor: "black"
+    borderRadius: 12,
+    backgroundColor: AppStyles.darkColor
   }
 })
 
