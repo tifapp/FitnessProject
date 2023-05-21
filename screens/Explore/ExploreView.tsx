c2060b1c7871faf152cc8c99bad90de77cdcc3f4
Matthew Hayes <matthewha853@gmail.com>
2023-05-20 21:52:46 -0700

    Explore screen user location fetch

--- screens/Explore/ExploreView.tsx
+++ screens/Explore/ExploreView.tsx
@@ -1,5 +1,46 @@
+import { CurrentUserEvent } from "@lib/events"
+import {
+  LocationCoordinate2D,
+  TrackedLocationCoordinates,
+  expoQueryUserCoordinates
+} from "@lib/location"
 import React from "react"
 import { StyleProp, ViewStyle } from "react-native"
+import {
+  LocationPermissionResponse,
+  requestForegroundPermissionsAsync
+} from "expo-location"
+
+const exploreQueryUserLocation = async () => {
+  return await expoQueryUserCoordinates("approximate-low")
+}
+
+/**
+ * Fetches the user location on the explore screen by first requesting
+ * for foreground location permissions.
+ */
+export const exploreFetchUserLocation = async (
+  requestPermission: () => Promise<LocationPermissionResponse> = requestForegroundPermissionsAsync,
+  fetchLocation: () => Promise<TrackedLocationCoordinates> = exploreQueryUserLocation
+) => {
+  const didGrantLocationPermissions = await requestPermission().then(
+    (res) => res.granted
+  )
+
+  if (!didGrantLocationPermissions) {
+    return { status: "permission-denied" }
+  }
+
+  return { status: "success", location: await fetchLocation() }
+}
+
+export type ExlporeEnvironment = {
+  fetchEvents: (
+    center: LocationCoordinate2D,
+    radiusMeters: number
+  ) => Promise<CurrentUserEvent>
+  fetchUserLocation: () => Promise<LocationCoordinate2D>
+}
 
 export const useExplore = () => {}
 
--- tests/Explore.test.tsx
+++ tests/Explore.test.tsx
@@ -1,3 +1,27 @@
+import { mockTrackedLocationCoordinate } from "@lib/location"
+import { exploreFetchUserLocation } from "@screens/Explore"
+
 describe("Explore tests", () => {
+  describe("exploreFetchUserLocation tests", () => {
+    it("returns a permissions denied status when location permission request is denied", async () => {
+      expect(
+        await exploreFetchUserLocation(
+          jest.fn().mockResolvedValue({ granted: false }),
+          jest.fn()
+        )
+      ).toMatchObject({ status: "permission-denied" })
+    })
+
+    it("loads the user's location when location permission request is granted", async () => {
+      const trackedCoordinate = mockTrackedLocationCoordinate()
+      expect(
+        await exploreFetchUserLocation(
+          jest.fn().mockResolvedValue({ granted: true }),
+          jest.fn().mockResolvedValue(trackedCoordinate)
+        )
+      ).toMatchObject({ status: "success", location: trackedCoordinate })
+    })
+  })
+
   describe("useExplore tests", () => {})
 })
