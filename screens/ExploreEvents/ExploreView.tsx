3f48ddb44da7fcef8f689bdfb50cff15248f230b
Matthew Hayes <matthewha853@gmail.com>
2023-05-24 17:37:49 -0700

    Ensure in loading state when events being fetched

--- screens/ExploreEvents/ExploreView.tsx
+++ screens/ExploreEvents/ExploreView.tsx
@@ -1,143 +1,22 @@
 import { CurrentUserEvent } from "@lib/events"
-import {
-  LocationCoordinate2D,
-  TrackedLocationCoordinates,
-  expoQueryUserCoordinates
-} from "@lib/location"
-import React, { useEffect } from "react"
+import { LocationCoordinate2D } from "@lib/location"
+import React, { useState } from "react"
 import { StyleProp, ViewStyle } from "react-native"
-import {
-  LocationPermissionResponse,
-  requestForegroundPermissionsAsync
-} from "expo-location"
 import { useQuery } from "react-query"
-import { milesToMeters } from "@lib/Math"
 
-const exploreEventsQueryUserLocation = async () => {
-  return await expoQueryUserCoordinates("approximate-low")
-}
-
-export type ExploreEventsUserLocation =
-  | { status: "permission-denied"; location: undefined }
-  | { status: "success"; location: TrackedLocationCoordinates }
-
-/**
- * Fetches the user location on the explore screen by first requesting
- * for foreground location permissions.
- */
-export const exploreEventsFetchUserLocation = async (
-  requestPermission: () => Promise<LocationPermissionResponse> = requestForegroundPermissionsAsync,
-  fetchLocation: () => Promise<TrackedLocationCoordinates> = exploreEventsQueryUserLocation
-): Promise<ExploreEventsUserLocation> => {
-  const didGrantLocationPermissions = await requestPermission().then(
-    (res) => res.granted
-  )
-
-  if (!didGrantLocationPermissions) {
-    return { status: "permission-denied", location: undefined }
-  }
-
-  return { status: "success", location: await fetchLocation() }
-}
-
-export type ExploreEventsError =
-  | "user-location"
-  | "events-loading"
-  | "no-results"
-
-export type ExploreEventsData =
-  | { status: "error"; type: ExploreEventsError }
-  | { status: "success"; events: CurrentUserEvent[] }
-  | { status: "loading" }
-
-export type UseExploreEventsResult = {
-  data: ExploreEventsData
-}
-
-export type UseExploreEventsProps = {
-  center?: LocationCoordinate2D
-  fetchEvents?: (center: LocationCoordinate2D) => Promise<CurrentUserEvent[]>
-  fetchUserLocation?: () => Promise<ExploreEventsUserLocation>
-  onUserLocationPermissionDenied: () => void
-}
-
-export const useExploreEvents = ({
-  center,
-  fetchEvents = async () => [] as CurrentUserEvent[],
-  fetchUserLocation = exploreEventsFetchUserLocation,
-  onUserLocationPermissionDenied
-}: UseExploreEventsProps): UseExploreEventsResult => {
-  const data = useExploreEventsData({
-    center,
-    fetchEvents,
-    fetchUserLocation,
-    onUserLocationPermissionDenied
-  })
-  return { data }
-}
-
-type UseExploreEventsDataProps = {
-  center?: LocationCoordinate2D
-  fetchEvents: (center: LocationCoordinate2D) => Promise<CurrentUserEvent[]>
-  fetchUserLocation: () => Promise<ExploreEventsUserLocation>
-  onUserLocationPermissionDenied: () => void
-}
-
-const useExploreEventsData = ({
-  center,
-  fetchEvents,
-  fetchUserLocation,
-  onUserLocationPermissionDenied
-}: UseExploreEventsDataProps): ExploreEventsData => {
-  const { data: userLocation, isError: isLocationError } =
-    useExploreEventsUserLocation(
-      fetchUserLocation,
-      onUserLocationPermissionDenied,
-      !center
-    )
-
-  const { data: events, isError: isEventsError } = useEventsQuery(
-    center ?? userLocation?.location?.coordinates,
-    fetchEvents
-  )
-
-  if (isLocationError) return { status: "error", type: "user-location" }
-  if (isEventsError) return { status: "error", type: "events-loading" }
-  if (!events) return { status: "loading" }
-  return events.length === 0
-    ? { status: "error", type: "no-results" }
-    : { status: "success", events }
-}
-
-const useEventsQuery = (
-  center: LocationCoordinate2D | undefined,
+export const useExploreEvents = (
   fetchEvents: (center: LocationCoordinate2D) => Promise<CurrentUserEvent[]>
 ) => {
-  return useQuery(
+  const [center, setCenter] = useState<LocationCoordinate2D | undefined>()
+  const events = useQuery(
     ["explore-events", center],
-    // NB: We must have a center for this query to be enabled, so the force unwrap is safe
     async () => await fetchEvents(center!),
     { enabled: !!center }
   )
-}
-
-const useExploreEventsUserLocation = (
-  fetchLocation: () => Promise<ExploreEventsUserLocation>,
-  onPermissionDenied: () => void,
-  isEnabled: boolean
-) => {
-  const query = useQuery(
-    ["explore-events-user-location"],
-    async () => await fetchLocation(),
-    { enabled: isEnabled }
-  )
-  const { data } = query
-  useEffect(() => {
-    if (data?.status === "permission-denied") {
-      onPermissionDenied()
-    }
-  }, [data?.status, onPermissionDenied])
-  return query
+  return {
+    events,
+    regionUpdated: setCenter
+  }
 }
 
 export type ExploreEventsProps = {
--- tests/ExploreEvents.test.tsx
+++ tests/ExploreEvents.test.tsx
@@ -1,151 +1,40 @@
-import {
-  LocationCoordinate2D,
-  mockLocationCoordinate2D,
-  mockTrackedLocationCoordinate
-} from "@lib/location"
-import {
-  exploreEventsFetchUserLocation,
-  useExploreEvents
-} from "@screens/ExploreEvents"
+import { mockLocationCoordinate2D } from "@lib/location"
+import { useExploreEvents } from "@screens/ExploreEvents"
 import { renderHook, waitFor } from "@testing-library/react-native"
 import { TestQueryClientProvider } from "./helpers/ReactQuery"
-import { EventMocks } from "@lib/events"
+import { act } from "react-test-renderer"
+import { neverPromise } from "./helpers/Promise"
 
 describe("ExploreEvents tests", () => {
   beforeEach(() => jest.resetAllMocks())
 
-  describe("exploreEventsFetchUserLocation tests", () => {
-    it("returns a permissions denied status when location permission request is denied", async () => {
-      expect(
-        await exploreEventsFetchUserLocation(
-          jest.fn().mockResolvedValue({ granted: false }),
-          jest.fn()
-        )
-      ).toMatchObject({ status: "permission-denied" })
-    })
-
-    it("loads the user's location when location permission request is granted", async () => {
-      const trackedCoordinate = mockTrackedLocationCoordinate()
-      expect(
-        await exploreEventsFetchUserLocation(
-          jest.fn().mockResolvedValue({ granted: true }),
-          jest.fn().mockResolvedValue(trackedCoordinate)
-        )
-      ).toMatchObject({ status: "success", location: trackedCoordinate })
-    })
-  })
-
   describe("useExploreEvents tests", () => {
-    it("presents the location search when user location permissions denied", async () => {
-      fetchUserLocation.mockResolvedValue({ status: "permission-denied" })
-      renderUseExploreEvents()
+    beforeEach(() => jest.useFakeTimers())
+    afterEach(() => jest.useRealTimers())
 
-      await waitFor(() => expect(fetchUserLocation).toHaveBeenCalled())
-      expect(onUserLocationPermissionDenied).toHaveBeenCalled()
-    })
+    it("should be in a loading state when fetching events", async () => {
+      fetchEvents.mockImplementation(neverPromise)
+      const { result } = renderUseExploreEvents()
+      expect(result.current.events).toMatchObject({ status: "idle" })
 
-    it("does not present location search when user location fetch successful", async () => {
-      const location = mockTrackedLocationCoordinate()
-      fetchUserLocation.mockResolvedValue({ status: "success", location })
-      renderUseExploreEvents()
+      const coordinates = mockLocationCoordinate2D()
+      act(() => result.current.regionUpdated(coordinates))
 
       await waitFor(() => {
-        expect(onUserLocationPermissionDenied).not.toHaveBeenCalled()
+        expect(result.current.events).toMatchObject({
+          status: "loading"
+        })
       })
     })
 
-    it("presents a user location error when location fetch fails and no preset center", async () => {
-      fetchUserLocation.mockRejectedValue(new Error("The user died"))
-      const { result } = renderUseExploreEvents()
-
-      expect(result.current.data).toMatchObject({ status: "loading" })
-      await waitFor(() => expect(fetchUserLocation).toHaveBeenCalled())
-      expect(result.current.data).toMatchObject({
-        status: "error",
-        type: "user-location"
-      })
-    })
-
-    it("does not fetch user location when initial center given", async () => {
-      const center = mockLocationCoordinate2D()
-      renderUseExploreEvents(center)
-      await waitFor(() => expect(fetchUserLocation).not.toHaveBeenCalled())
-    })
-
-    it("fetches events with the initial given center", async () => {
-      const center = mockLocationCoordinate2D()
-      fetchEvents.mockResolvedValue([EventMocks.PickupBasketball])
-      const { result } = renderUseExploreEvents(center)
-
-      expect(result.current.data).toMatchObject({ status: "loading" })
-      await waitFor(() => expect(fetchEvents).toHaveBeenCalledWith(center))
-      expect(result.current.data).toMatchObject({
-        status: "success",
-        events: [EventMocks.PickupBasketball]
-      })
-    })
-
-    it("fetches events with user location as center when no initial center given", async () => {
-      const location = mockTrackedLocationCoordinate()
-      fetchUserLocation.mockResolvedValue({ status: "success", location })
-      fetchEvents.mockResolvedValue([EventMocks.PickupBasketball])
-      const { result } = renderUseExploreEvents()
-
-      expect(result.current.data).toMatchObject({ status: "loading" })
-      await waitFor(() => expect(fetchUserLocation).toHaveBeenCalled())
-      expect(fetchEvents).toHaveBeenCalledWith(location.coordinates)
-      expect(result.current.data).toMatchObject({
-        status: "success",
-        events: [EventMocks.PickupBasketball]
-      })
-    })
-
-    it("presents a no-results error when empty events array fetched", async () => {
-      fetchEvents.mockResolvedValue([])
-      const { result } = renderUseExploreEvents(mockLocationCoordinate2D())
-
-      expect(result.current.data).toMatchObject({ status: "loading" })
-      await waitFor(() => expect(fetchEvents).toHaveBeenCalled())
-      expect(result.current.data).toMatchObject({
-        status: "error",
-        type: "no-results"
-      })
-    })
-
-    it("presents an events-loading error when event loading fails", async () => {
-      fetchEvents.mockRejectedValue(
-        new Error("Our backend was destroyed by a meteor")
-      )
-      const { result } = renderUseExploreEvents(mockLocationCoordinate2D())
-
-      expect(result.current.data).toMatchObject({ status: "loading" })
-      await waitFor(() => expect(fetchEvents).toHaveBeenCalled())
-      expect(result.current.data).toMatchObject({
-        status: "error",
-        type: "events-loading"
-      })
-    })
-
-    const onUserLocationPermissionDenied = jest.fn()
     const fetchEvents = jest.fn()
-    const fetchUserLocation = jest.fn()
 
-    const renderUseExploreEvents = (center?: LocationCoordinate2D) => {
-      return renderHook(
-        () => {
-          return useExploreEvents({
-            center,
-            fetchEvents,
-            fetchUserLocation,
-            onUserLocationPermissionDenied
-          })
-        },
-        {
-          wrapper: ({ children }) => (
-            <TestQueryClientProvider>{children}</TestQueryClientProvider>
-          )
-        }
-      )
+    const renderUseExploreEvents = () => {
+      return renderHook(() => useExploreEvents(fetchEvents), {
+        wrapper: ({ children }) => (
+          <TestQueryClientProvider>{children}</TestQueryClientProvider>
+        )
+      })
     }
   })
 })
