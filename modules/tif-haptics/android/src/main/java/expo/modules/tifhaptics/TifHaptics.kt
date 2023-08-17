4d7445705e5901ad0957514d8cb91674541b7388
mhayes853 <matthewha853@gmail.com>
2023-08-16 22:09:55 -0700

    Fix kotlin formatting

--- modules/tif-haptics/android/src/main/java/expo/modules/tifhaptics/TifHaptics.kt
+++ modules/tif-haptics/android/src/main/java/expo/modules/tifhaptics/TifHaptics.kt
@@ -8,42 +8,42 @@ import kotlinx.coroutines.sync.withLock
 import expo.modules.kotlin.types.Enumerable
 
 enum class HapticEvent(val value: String) : Enumerable {
-	SELECTION("selection")
+  SELECTION("selection")
 }
 
 /**
  * A haptics player for android.
  */
 class TiFHapticsEngine(private val vibrator: Vibrator) {
-	private var isMuted = false
-	private val mutex = Mutex()
+  private var isMuted = false
+  private val mutex = Mutex()
 
-	suspend fun play(event: HapticEvent) = mutex.withLock {
-		if (isMuted) return
-		when (event) {
-			HapticEvent.SELECTION -> this.playSelection()
-		}
-	}
+  suspend fun play(event: HapticEvent) = mutex.withLock {
+    if (isMuted) return
+    when (event) {
+      HapticEvent.SELECTION -> this.playSelection()
+    }
+  }
 
-	private fun playSelection() {
-		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-			vibrator.vibrate(
-				VibrationEffect.createWaveform(
-					longArrayOf(0, 100),
-					intArrayOf(0, 100),
-					-1
-				)
-			)
-		} else {
-			vibrator.vibrate(longArrayOf(0, 70), -1)
-		}
-	}
+  private fun playSelection() {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+      vibrator.vibrate(
+        VibrationEffect.createWaveform(
+          longArrayOf(0, 100),
+          intArrayOf(0, 100),
+          -1
+        )
+      )
+    } else {
+      vibrator.vibrate(longArrayOf(0, 70), -1)
+    }
+  }
 
-	suspend fun mute() = mutex.withLock {
-		isMuted = true
-	}
+  suspend fun mute() = mutex.withLock {
+    isMuted = true
+  }
 
-	suspend fun unmute() = mutex.withLock {
-		isMuted = false
-	}
+  suspend fun unmute() = mutex.withLock {
+    isMuted = false
+  }
 }
