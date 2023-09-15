577d8ec507d7d649af423cb8e0c5b26d6ac67b23
mhayes853 <matthewha853@gmail.com>
2023-09-14 18:10:32 -0700

    Add ability to focus text fields

--- components/TextFields.tsx
+++ components/TextFields.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode, useState } from "react"
+import React, { ReactNode, useEffect, useRef, useState } from "react"
 import {
   TextInputProps,
   StyleSheet,
@@ -16,6 +16,7 @@ export type TextFieldProps = {
   leftAddon?: JSX.Element
   rightAddon?: JSX.Element
   error?: ReactNode
+  isFocused?: boolean
   textStyle?: StyleProp<TextStyle>
   style?: StyleProp<ViewStyle>
 } & Omit<TextInputProps, "style" | "placeholderStyle">
@@ -23,37 +24,17 @@ export type TextFieldProps = {
 /**
  * A generic Text Field component.
  */
-export const TextField = ({
-  leftAddon,
-  rightAddon,
-  error,
-  textStyle,
-  style,
-  ...props
-}: TextFieldProps) => {
+export const TextField = ({ error, style, ...props }: TextFieldProps) => {
   const borderColor = error ? AppStyles.errorColor : "rgba(0, 0, 0, 0.10)"
   return (
     <View style={style}>
       <View style={[styles.card, { borderColor }]}>
-        <View style={styles.container}>
-          <View style={styles.leftAddon}>{leftAddon}</View>
-          <View style={styles.leftContainer}>
-            <TextInput
-              style={[styles.textInput, textStyle]}
-              placeholderTextColor={AppStyles.colorOpacity35}
-              {...props}
-            />
-          </View>
-          <View style={styles.rightAddon}>{rightAddon}</View>
-        </View>
+        <InternalTextField
+          placeholderTextColor={AppStyles.colorOpacity35}
+          {...props}
+        />
       </View>
-      {typeof error === "string" || typeof error === "number"
-        ? (
-          <Caption style={styles.errorText}>{error}</Caption>
-        )
-        : (
-          error
-        )}
+      <TextFieldErrorView error={error} />
     </View>
   )
 }
@@ -61,28 +42,20 @@ export const TextField = ({
 /**
  * A text field with a filled background.
  */
-export const ShadedTextField = ({
-  leftAddon,
-  rightAddon,
-  error,
-  textStyle,
-  style,
-  ...props
-}: TextFieldProps) => (
+export const ShadedTextField = ({ error, style, ...props }: TextFieldProps) => (
   <View style={style}>
     <View style={[styles.filledCard]}>
-      <View style={styles.container}>
-        <View style={styles.leftAddon}>{leftAddon}</View>
-        <View style={styles.leftContainer}>
-          <TextInput
-            style={[styles.textInput, textStyle]}
-            placeholderTextColor={AppStyles.colorOpacity50}
-            {...props}
-          />
-        </View>
-        <View style={styles.rightAddon}>{rightAddon}</View>
-      </View>
+      <InternalTextField
+        placeholderTextColor={AppStyles.colorOpacity35}
+        {...props}
+      />
     </View>
+    <TextFieldErrorView error={error} />
+  </View>
+)
+
+const TextFieldErrorView = ({ error }: { error?: ReactNode }) => (
+  <>
     {typeof error === "string" || typeof error === "number"
       ? (
         <Caption style={styles.errorText}>{error}</Caption>
@@ -90,8 +63,41 @@ export const ShadedTextField = ({
       : (
         error
       )}
-  </View>
+  </>
 )
+
+const InternalTextField = ({
+  leftAddon,
+  rightAddon,
+  textStyle,
+  isFocused,
+  ...props
+}: TextFieldProps) => {
+  const ref = useRef<TextInput>(null)
+
+  useEffect(() => {
+    if (isFocused) {
+      ref.current?.focus()
+    } else {
+      ref.current?.blur()
+    }
+  }, [isFocused])
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.leftAddon}>{leftAddon}</View>
+      <View style={styles.leftContainer}>
+        <TextInput
+          ref={ref}
+          style={[styles.textInput, textStyle]}
+          placeholderTextColor={AppStyles.colorOpacity50}
+          {...props}
+        />
+      </View>
+      <View style={styles.rightAddon}>{rightAddon}</View>
+    </View>
+  )
+}
 
 export type PasswordTextFieldProps = Omit<
   TextFieldProps,
@@ -101,42 +107,42 @@ export type PasswordTextFieldProps = Omit<
 /**
  * A text field component for password inputs.
  */
-export const PasswordTextField = ({ ...props }: PasswordTextFieldProps) => {
-  const [isShowingPassword, setIsShowingPassword] = useState(false)
-  return (
-    <TextField
-      secureTextEntry={!isShowingPassword}
-      rightAddon={
-        <TouchableIonicon
-          icon={{ name: isShowingPassword ? "eye" : "eye-off" }}
-          onPress={() => {
-            setIsShowingPassword((isShowing) => !isShowing)
-          }}
-          accessibilityLabel={
-            isShowingPassword ? "Hide password" : "Show password"
-          }
-        />
-      }
-      {...props}
-    />
-  )
-}
+export const PasswordTextField = ({ ...props }: PasswordTextFieldProps) => (
+  <InternalPasswordTextField TextFieldView={TextField} {...props} />
+)
 
 /**
  * A password text field which is filled with a solic background
  */
 export const ShadedPasswordTextField = ({
   ...props
-}: PasswordTextFieldProps) => {
+}: PasswordTextFieldProps) => (
+  <InternalPasswordTextField
+    TextFieldView={ShadedTextField}
+    iconColor={AppStyles.colorOpacity50}
+    {...props}
+  />
+)
+
+type InternalPasswordTextFieldProps = {
+  TextFieldView: typeof TextField
+  iconColor?: string
+} & PasswordTextFieldProps
+
+const InternalPasswordTextField = ({
+  TextFieldView,
+  iconColor,
+  ...props
+}: InternalPasswordTextFieldProps) => {
   const [isShowingPassword, setIsShowingPassword] = useState(false)
   return (
-    <ShadedTextField
+    <TextFieldView
       secureTextEntry={!isShowingPassword}
       rightAddon={
         <TouchableIonicon
           icon={{
             name: isShowingPassword ? "eye" : "eye-off",
-            color: AppStyles.colorOpacity50
+            color: iconColor
           }}
           onPress={() => {
             setIsShowingPassword((isShowing) => !isShowing)
