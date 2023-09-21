e36bacc83f0db9c267774d3bdfbe8c13e7513aff
ZoroarkZark <ilikepie1464@gmail.com>
2023-09-21 13:55:12 -0700

    Fix up mock

--- tests/helpers/SecureStorage.ts
+++ tests/helpers/SecureStorage.ts
@@ -4,60 +4,6 @@ import * as ExpoSecureStore from "expo-secure-store"
 export class TestSecureStorage implements SecureStore {
   private TestAmplifyStore = new Map<string, string>()
 
-  // @needsAudit
-  /**
-   * The data in the keychain item cannot be accessed after a restart until the device has been
-   * unlocked once by the user. This may be useful if you need to access the item when the phone
-   * is locked.
-   */
-  AFTER_FIRST_UNLOCK: ExpoSecureStore.KeychainAccessibilityConstant =
-    ExpoSecureStore.AFTER_FIRST_UNLOCK
-
-  // @needsAudit
-  /**
-   * Similar to `AFTER_FIRST_UNLOCK`, except the entry is not migrated to a new device when restoring
-   * from a backup.
-   */
-  AFTER_FIRST_UNLOCK_THIS_DEVICE_ONLY: ExpoSecureStore.KeychainAccessibilityConstant =
-    ExpoSecureStore.AFTER_FIRST_UNLOCK_THIS_DEVICE_ONLY
-
-  // @needsAudit
-  /**
-   * The data in the keychain item can always be accessed regardless of whether the device is locked.
-   * This is the least secure option.
-   */
-  ALWAYS: ExpoSecureStore.KeychainAccessibilityConstant = ExpoSecureStore.ALWAYS
-
-  // @needsAudit
-  /**
-   * Similar to `WHEN_UNLOCKED_THIS_DEVICE_ONLY`, except the user must have set a passcode in order to
-   * store an entry. If the user removes their passcode, the entry will be deleted.
-   */
-  WHEN_PASSCODE_SET_THIS_DEVICE_ONLY: ExpoSecureStore.KeychainAccessibilityConstant =
-    ExpoSecureStore.WHEN_PASSCODE_SET_THIS_DEVICE_ONLY
-
-  // @needsAudit
-  /**
-   * Similar to `ALWAYS`, except the entry is not migrated to a new device when restoring from a backup.
-   */
-  ALWAYS_THIS_DEVICE_ONLY: ExpoSecureStore.KeychainAccessibilityConstant =
-    ExpoSecureStore.ALWAYS_THIS_DEVICE_ONLY
-
-  // @needsAudit
-  /**
-   * The data in the keychain item can be accessed only while the device is unlocked by the user.
-   */
-  WHEN_UNLOCKED: ExpoSecureStore.KeychainAccessibilityConstant =
-    ExpoSecureStore.WHEN_UNLOCKED
-
-  // @needsAudit
-  /**
-   * Similar to `WHEN_UNLOCKED`, except the entry is not migrated to a new device when restoring from
-   * a backup.
-   */
-  WHEN_UNLOCKED_THIS_DEVICE_ONLY: ExpoSecureStore.KeychainAccessibilityConstant =
-    ExpoSecureStore.WHEN_UNLOCKED_THIS_DEVICE_ONLY
-
   async setItemAsync (
     key: string,
     value: string,
@@ -72,9 +18,5 @@ export class TestSecureStorage implements SecureStore {
 
   async deleteItemAsync (key: string) {
     this.TestAmplifyStore.delete(key)
-  }
-
-  async isAvailableAsync () {
-    return true
   }
 }
