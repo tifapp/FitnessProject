00efd055252ae46ebab34a6ddb7175c16d8f2a14
ZoroarkZark <ilikepie1464@gmail.com>
2023-10-13 20:35:47 -0700

    Navigation update for CI

--- auth/sign-up/Navigation.tsx
+++ auth/sign-up/Navigation.tsx
@@ -1,30 +1,30 @@
-import React, { memo, useRef } from "react"
+import {
+  AuthVerificationCodeFormView,
+  useAuthVerificationCodeForm
+} from "@auth/VerifyCode"
 import {
   BASE_HEADER_SCREEN_OPTIONS,
   ChevronBackButton,
   StackNavigatorType,
   XMarkBackButton
 } from "@components/Navigation"
-import { EmailAddress, USPhoneNumber } from ".."
+import { TouchableIonicon } from "@components/common/Icons"
 import { UserHandle } from "@lib/users"
+import { NavigatorScreenParams, useNavigation } from "@react-navigation/native"
 import { StackScreenProps, createStackNavigator } from "@react-navigation/stack"
-import {
-  SignUpCredentialsFormView,
-  useSignUpCredentialsForm
-} from "./CredentialsForm"
+import React, { memo } from "react"
+import { Alert, StyleSheet } from "react-native"
+import { EmailAddress, USPhoneNumber } from ".."
 import {
   SignUpChangeUserHandleFormView,
   useSignUpChangeUserHandleForm
 } from "./ChangeUserHandle"
-import { SignUpEndingView } from "./Ending"
-import { NavigatorScreenParams, useNavigation } from "@react-navigation/native"
 import {
-  useAuthVerificationCodeForm,
-  AuthVerificationCodeFormView
-} from "@auth/VerifyCode"
+  SignUpCredentialsFormView,
+  useSignUpCredentialsForm
+} from "./CredentialsForm"
+import { SignUpEndingView } from "./Ending"
 import { SignUpEnvironment } from "./Environment"
-import { TouchableIonicon } from "@components/common/Icons"
-import { Alert, StyleSheet } from "react-native"
 
 export type SignUpModalParamsList = {
   signUpCredentialsForm: undefined
@@ -161,7 +161,6 @@ const VerifyCodeFormScreen = memo(function Screen ({
   finishRegisteringAccount,
   resendVerificationCode
 }: VerifyCodeFormScreenProps) {
-  const generatedUserHandleRef = useRef<UserHandle | undefined>()
   const form = useAuthVerificationCodeForm({
     resendCode: async () =>
       await resendVerificationCode(route.params.emailOrPhoneNumber),
@@ -171,15 +170,15 @@ const VerifyCodeFormScreen = memo(function Screen ({
         code
       )
       if (result === "invalid-verification-code") {
-        return false
+        return { isCorrect: false }
+      } else {
+        return { isCorrect: true, data: result }
       }
-      generatedUserHandleRef.current = result
-      return true
     },
-    onSuccess: () => {
-      if (generatedUserHandleRef.current) {
+    onSuccess: (data) => {
+      if (data) {
         navigation.replace("signUpChangeUserHandleForm", {
-          initialHandle: generatedUserHandleRef.current
+          initialHandle: data
         })
       }
     }
