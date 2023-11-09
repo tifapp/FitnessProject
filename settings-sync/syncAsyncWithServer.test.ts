24e6b9cf16b897456f142264c6e29af5b671390f
ZoroarkZark <ilikepie1464@gmail.com>
2023-11-08 18:14:49 -0800

    Beginning of structure

---
+++ settings-sync/syncAsyncWithServer.test.ts
@@ -0,0 +1,7 @@
+describe("syncAsyncWithServer tests", () => {
+  describe("settingsSync tests", () => {
+    test("recognises difference in dates: async greater than server = replace data", () => {})
+    test("should take object from async and compare with server object", () => {})
+    test("settingsSync compares data and ")
+  })
+})
