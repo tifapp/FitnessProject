e9fd92a22f8176603edd9d16bec1d60a703ec784
mhayes853 <matthewha853@gmail.com>
2023-11-11 00:52:53 -0800

    Read more text

--- components/ContentText.tsx
+++ components/ContentText.tsx
@@ -1,11 +1,20 @@
-import React, { useMemo } from "react"
-import { StyleSheet, TextProps } from "react-native"
+import React, { useMemo, useRef, useState } from "react"
+import {
+  StyleProp,
+  StyleSheet,
+  TextProps,
+  TextStyle,
+  TouchableOpacity,
+  View,
+  ViewStyle
+} from "react-native"
 import { openURL } from "expo-linking"
 import { BodyText, Headline } from "./Text"
 import { linkify } from "@lib/Linkify"
 import { Match } from "linkify-it"
 import { UserHandle, UserHandleLinkifyMatch } from "@lib/users"
 import { EventHandle, EventHandleLinkifyMatch } from "@event-details"
+import Animated, { FadeIn, Layout } from "react-native-reanimated"
 
 export type ContentTextCallbacks = {
   onUserHandleTapped: (handle: UserHandle) => void
@@ -49,6 +58,94 @@ export const ContentText = ({
     )}
   </BodyText>
 )
+
+export type ExpandableContentTextProps = {
+  collapsedLineLimit: number
+  initialText: string
+  contentTextStyle?: StyleProp<TextStyle>
+  expandButtonTextStyle?: StyleProp<TextStyle>
+  style?: StyleProp<ViewStyle>
+} & Omit<ContentTextProps, "numberOfLines" | "text" | "style">
+
+/**
+ * Content text that expands when the user presses a "Read More" button.
+ *
+ * At the moment, this only renders the initial text given to it correctly, it does not
+ * render any changes to the input text.
+ */
+export const ExpandableContentText = ({
+  collapsedLineLimit,
+  onTextLayout,
+  initialText,
+  contentTextStyle,
+  expandButtonTextStyle,
+  style,
+  ...props
+}: ExpandableContentTextProps) => {
+  const [isExpanded, setIsExpanded] = useState(false)
+  const [expansionStatus, setExpansionStatus] = useState<
+    "expandable" | "unexpandable" | undefined
+  >()
+  const textSplitsRef = useRef({
+    collapsedText: "",
+    expandedText: "",
+    originalText: ""
+  })
+  const numberOfLines = expansionStatus
+    ? collapsedLineLimit
+    : collapsedLineLimit + 1
+  return (
+    <View style={style}>
+      <ContentText
+        {...props}
+        text={isExpanded ? textSplitsRef.current.collapsedText : initialText}
+        numberOfLines={isExpanded ? undefined : numberOfLines}
+        onTextLayout={(e) => {
+          if (expansionStatus) {
+            onTextLayout?.(e)
+            return
+          }
+          const textBlocks = e.nativeEvent.lines.map((line) => line.text)
+          textSplitsRef.current.originalText = initialText
+          const visibleText = textBlocks
+            .slice(0, textBlocks.length - 1)
+            .join("")
+          textSplitsRef.current.collapsedText = visibleText.endsWith("\n")
+            ? visibleText.slice(0, visibleText.length - 1)
+            : visibleText
+          textSplitsRef.current.expandedText = initialText.slice(
+            visibleText.length
+          )
+          setExpansionStatus(
+            textBlocks.length !== collapsedLineLimit + 1
+              ? "unexpandable"
+              : "expandable"
+          )
+          onTextLayout?.(e)
+        }}
+        style={[contentTextStyle, { opacity: expansionStatus ? 1 : 0 }]}
+      />
+      {isExpanded && (
+        <Animated.View entering={FadeIn.duration(300)}>
+          <ContentText {...props} text={textSplitsRef.current.expandedText} />
+        </Animated.View>
+      )}
+      {expansionStatus === "expandable" && (
+        <Animated.View layout={Layout.duration(300)}>
+          <TouchableOpacity
+            onPress={() => setIsExpanded((expanded) => !expanded)}
+            style={styles.readMore}
+            hitSlop={44}
+          >
+            <Headline style={expandButtonTextStyle}>
+              {isExpanded ? "Read Less" : "Read More"}
+            </Headline>
+          </TouchableOpacity>
+        </Animated.View>
+      )}
+    </View>
+  )
+}
 
 const useTextBlocks = (text: string, callbacks: ContentTextCallbacks) => {
   return useMemo(() => renderLinkTextBlocks(text, callbacks), [text, callbacks])
@@ -119,5 +216,8 @@ const styles = StyleSheet.create({
   },
   handle: {
     color: "#4285F4"
+  },
+  readMore: {
+    marginTop: 8
   }
 })
