cf972b5ee5270bd2fd2894f7f1973b330659f7dd
tifapp <tifapppublic@gmail.com>
2023-11-17 22:49:41 -0800

    freddy

--- easbuildsuccess.mjs
+++ easbuildsuccess.mjs
@@ -1,43 +1,40 @@
 import dotenv from "dotenv"
-import nodeFetch from "node-fetch"
+import jwt from "jsonwebtoken"
+import { Octokit } from "octokit"
 
 dotenv.config()
-console.log(JSON.stringify(process.env, null, 4))
-;(async () => {
-  console.log(JSON.stringify(process.env, null, 4))
-  const token = process.env.GH_TOKEN
-  const repoFullName = process.env.GITHUB_REPOSITORY
-  const apiUrl = `https://api.github.com/repos/${repoFullName}/check-runs/${process.env.CHECK_RUN_ID}`
+const jwtToken = jwt.sign(
+  {
+    iat: Math.floor(Date.now() / 1000),
+    exp: Math.floor(Date.now() / 1000) + 10 * 60,
+    iss: process.env.GITHUB_APP_ID
+  },
+  process.env.GITHUB_APP_PRIVATE_KEY ?? "",
+  { algorithm: "RS256" }
+)
 
-  const response = await nodeFetch(apiUrl, {
-    method: "PATCH",
-    headers: {
-      Authorization: `token ${token}`,
-      Accept: "application/vnd.github.v3+json",
-      "Content-Type": "application/json",
-      "X-GitHub-Api-Version": "2022-11-28"
-    },
-    body: JSON.stringify({
+const octokit = new Octokit({ auth: jwtToken })
+
+const updateCheckRun = async () => {
+  try {
+    const apiUrl = `/repos/${process.env.GITHUB_REPOSITORY}/check-runs/${process.env.CHECK_RUN_ID}`
+    const response = await octokit.request(`PATCH ${apiUrl}`, {
+      owner: "tifapp",
+      repo: "FitnessProject",
+      check_run_id: process.env.CHECK_RUN_ID,
       name: "EAS Build",
-      head_sha: process.env.GITHUB_SHA,
       status: "completed",
       conclusion: "success",
       completed_at: new Date().toISOString(),
       output: {
-        title: "EAS Build Completed",
-        summary: "The build has completed.",
-        text: "Build completed with conclusion: success."
+        title: "Build Result",
+        summary: "Build completed successfully.",
+        text: "Detailed build information here."
       }
     })
-  })
-
-  if (!response.ok) {
-    const errorDetails = await response.text()
-    throw new Error(
-      `GitHub API responded with ${response.status}: ${errorDetails}`
-    )
+  } catch (error) {
+    console.error("Error updating check run:", error)
   }
+}
 
-  const data = await response.json()
-  return data
-})()
+updateCheckRun()
