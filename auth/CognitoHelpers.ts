b57614c049e5f232e49cd7d224caef46c879414c
mhayes853 <matthewha853@gmail.com>
2024-01-02 16:43:16 -0800

    Remove CognitoErrorCode type

--- auth/CognitoHelpers.ts
+++ auth/CognitoHelpers.ts
@@ -1,20 +1,6 @@
 import { AuthError } from "@aws-amplify/auth"
 
 /**
- * An error code thrown by cognito. Uses ConfirmForgotPasswordException, ForgotPasswordException, SignUpException, and InitiateAuthException
- * from the possible internal errors that can be caused if the parameter is accepted by Cognito.
- *
- */
-export type CognitoErrorCode =
-  | "CodeMismatchException"
-  | "InvalidPasswordException"
-  | "NotAuthorizedException"
-  | "TooManyFailedAttemptsException"
-  | "UserNotFoundException"
-  | "CodeDeliveryFailureException"
-  | "UsernameExistsException"
-
-/**
  * Returns true if the given error object is an instance of {@link AuthError} and has a property
  * `code` that matches the given Cognito error code.
  */
