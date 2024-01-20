23aa08e376e1eaa85adcc6fa62fd6bd8d7083182
tifapp <tifapppublic@gmail.com>
2024-01-20 15:26:38 -0800

    Clean up build variant config + use different names and icons

--- app.config.js
+++ app.config.js
@@ -5,29 +5,40 @@ dotenv.config({ path: ".env.infra" })
 const { MAPS_API_KEY, EXPO_PROJECT_ID, EXPO_PROJECT_OWNER, EAS_BUILD_TYPE } =
   process.env
 
-const bundleIdentifier =
+const { bundleIdentifier, googleServicesFile, icon, splash, name } =
   EAS_BUILD_TYPE === "development"
-    ? "com.tifapp.FitnessAppDevelopment"
+    ? {
+      bundleIdentifier: "com.tifapp.FitnessAppDevelopment",
+      googleServicesFile: "./development-google-services.json",
+      icon: ".storybook/assets/icon.png",
+      splash: ".storybook/assets/splash.png",
+      name: "FitnessAppDevelopment"
+    }
     : EAS_BUILD_TYPE === "preview"
-      ? "com.tifapp.FitnessAppPreview"
-      : "com.tifapp.FitnessApp"
-
-const googleServicesFile =
-  EAS_BUILD_TYPE === "development"
-    ? "./development-google-services.json"
-    : EAS_BUILD_TYPE === "preview"
-      ? "./preview-google-services.json"
-      : "./google-services.json"
+      ? {
+        bundleIdentifier: "com.tifapp.FitnessAppPreview",
+        googleServicesFile: "./preview-google-services.json",
+        icon: "./assets/icon.png",
+        splash: "./assets/splash.png",
+        name: "FitnessAppPreview"
+      }
+      : {
+        bundleIdentifier: "com.tifapp.FitnessApp",
+        googleServicesFile: "./google-services.json",
+        icon: "./assets/icon.png",
+        splash: "./assets/splash.png",
+        name: "FitnessApp"
+      }
 
 const config = {
-  name: "FitnessApp",
-  slug: "FitnessApp",
+  name,
+  slug: name,
   scheme: "tifapp",
   version: "1.0.0",
   orientation: "portrait",
-  icon: "./assets/icon.png",
+  icon,
   splash: {
-    image: "./assets/splash.png",
+    image: splash,
     resizeMode: "contain",
     backgroundColor: "#ffffff"
   },
@@ -72,9 +83,6 @@ const config = {
         "lftgpas"
       ]
     }
-  },
-  web: {
-    favicon: "./assets/favicon.png"
   },
   android: {
     package: bundleIdentifier,
