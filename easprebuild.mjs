13b07a7f7fb1458747e00fdfe3b46271d1855aa5
tifapp <tifapppublic@gmail.com>
2024-01-20 00:25:33 -0800

    Added missing easbuildtypes, removed unnecessary conditional when removing storybook from build (in local builds, your local bundle will be used instead)

--- easprebuild.mjs
+++ easprebuild.mjs
@@ -1,10 +1,8 @@
 import fs from "fs"
 
-if (process.env.EAS_BUILD_TYPE !== "development") {
-  const storybookPath = ".storybook"
+const directoryPath = "./.storybook"
 
-  if (fs.existsSync(storybookPath)) {
-    fs.rmdirSync(storybookPath, { recursive: true })
-    console.log(`${storybookPath} has been deleted`)
-  }
+if (fs.existsSync(directoryPath)) {
+  fs.rmdirSync(directoryPath, { recursive: true })
+  console.log(`${directoryPath} has been deleted`)
 }
