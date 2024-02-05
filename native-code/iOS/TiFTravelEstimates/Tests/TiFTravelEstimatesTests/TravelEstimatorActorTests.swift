e3e2b10a02fc618684bd786bc256d71835ed17bd
mhayes853 <matthewha853@gmail.com>
2024-02-05 02:37:57 -0800

    Initial Native code

---
+++ native-code/iOS/TiFTravelEstimates/Tests/TiFTravelEstimatesTests/TravelEstimatorActorTests.swift
@@ -0,0 +1,62 @@
+import XCTest
+import MapKit
+import TiFTravelEstimates
+
+final class TravelEstimatorActorTests: XCTestCase {
+  func testBasics() async throws {
+    let estimatesActor = TravelEstimatesActor { _, routeType in
+      switch routeType {
+      case .automobile: TravelEstimate(travelDistanceMeters: 1, expectedTravelSeconds: 2)
+      case .transit: TravelEstimate(travelDistanceMeters: 3, expectedTravelSeconds: 4)
+      case .walking: TravelEstimate(travelDistanceMeters: 5, expectedTravelSeconds: 6)
+      }
+    }
+    let estimates = try await estimatesActor.estimates(for: .treasureIsland)
+    XCTAssertEqual(
+      estimates,
+      TravelEstimates(
+        walking: TravelEstimate(travelDistanceMeters: 5, expectedTravelSeconds: 6),
+        transit: TravelEstimate(travelDistanceMeters: 3, expectedTravelSeconds: 4),
+        automobile: TravelEstimate(travelDistanceMeters: 1, expectedTravelSeconds: 2)
+      )
+    )
+  }
+  
+  func testMergesSimultaneousRequestsForSamePoints() async throws {
+    let expectation = self.expectation(description: "makes 3 individual route requests")
+    expectation.expectedFulfillmentCount = 3
+    let estimatesActor = TravelEstimatesActor { _, _ in
+      expectation.fulfill()
+      return TravelEstimate(travelDistanceMeters: 1, expectedTravelSeconds: 2)
+    }
+    async let estimates1 = estimatesActor.estimates(for: .treasureIsland)
+    async let estimates2 = estimatesActor.estimates(for: .treasureIsland)
+    let e1 = try await estimates1
+    let e2 = try await estimates2
+    XCTAssertEqual(e1, e2)
+    await self.fulfillment(of: [expectation], timeout: 0)
+  }
+  
+  @available(iOS 16, *)
+  func testCancellation() async throws {
+    let expectation = self.expectation(description: "starts task")
+    expectation.expectedFulfillmentCount = 3
+    let estimatesActor = TravelEstimatesActor { _, _ in
+      expectation.fulfill()
+      let neverStream = AsyncStream<TravelEstimate?> { _ in }
+      for await estimate in neverStream {
+        return estimate
+      }
+      throw CancellationError()
+    }
+    let task = Task { _ = try await estimatesActor.estimates(for: .treasureIsland) }
+    await self.fulfillment(of: [expectation], timeout: 0.05)
+    await estimatesActor.cancelEstimation(for: .treasureIsland)
+    do {
+      _ = try await task.value
+      XCTFail("should throw")
+    } catch {
+      XCTAssertTrue(error is CancellationError)
+    }
+  }
+}
--- native-code/iOS/TiFTravelEstimates/TiFTravelEstimates.podspec
+++ native-code/iOS/TiFTravelEstimates/TiFTravelEstimates.podspec
@@ -4,19 +4,8 @@ Pod::Spec.new do |s|
   s.summary        = 'A sample project summary'
   s.description    = 'A sample project description'
   s.author         = ''
-  s.homepage       = 'https://docs.expo.dev/modules/'
   s.platform       = :ios, '13.0'
   s.source         = { git: '' }
   s.static_framework = true
-
-  s.dependency 'ExpoModulesCore'
-  s.dependency
-
-  # Swift/Objective-C compatibility
-  s.pod_target_xcconfig = {
-    'DEFINES_MODULE' => 'YES',
-    'SWIFT_COMPILATION_MODE' => 'wholemodule'
-  }
-
-  s.source_files = "**/*.{h,m,mm,swift,hpp,cpp}"
+  s.source_files = 'Sources/*.swift'
 end
