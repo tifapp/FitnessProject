8c6ad68fe6acf31501ce9d7c14012be7bc4d6b98
mhayes853 <matthewha853@gmail.com>
2024-02-19 07:53:54 -0800

    Merge upgrade prettier

--- event-details/arrival-tracking/UpcomingArrivals.ts
+++ event-details/arrival-tracking/UpcomingArrivals.ts
@@ -1,10 +1,8 @@
-import AsyncStorage from "@react-native-async-storage/async-storage"
-import { AsyncStorageUtils } from "@lib/utils/AsyncStorage"
-import {
-  EventArrivalRegion,
-  EventArrivalRegionsSchema
-} from "@shared-models/EventArrivals"
+import { EventArrivalRegion } from "@shared-models/EventArrivals"
 import { getBackgroundPermissionsAsync } from "expo-location"
+import { TiFSQLite } from "@lib/SQLite"
+import { LocationCoordinate2D } from "@shared-models/Location"
+import { areEventRegionsEqual } from "@shared-models/Event"
 
 /**
  * An interface for storing client-side details on upcoming event arrivals.
@@ -20,25 +18,98 @@ export interface UpcomingEventArrivals {
    */
   replaceAll: (regions: EventArrivalRegion[]) => Promise<void>
 }
+
 /**
- * {@link UpcomingEventArrivals} implemented with {@link AsyncStorage}.
+ * {@link UpcomingEventArrivals} implemented with SQLite.
  */
-export class AsyncStorageUpcomingEventArrivals
-implements UpcomingEventArrivals {
-  private static KEY = "@upcoming-event-arrivals"
+export class SQLiteUpcomingEventArrivals implements UpcomingEventArrivals {
+  private readonly sqlite: TiFSQLite
+
+  constructor(sqlite: TiFSQLite) {
+    this.sqlite = sqlite
+  }
 
   async all() {
-    return await AsyncStorageUtils.parseJSONItem(
-      EventArrivalRegionsSchema,
-      AsyncStorageUpcomingEventArrivals.KEY
-    ).then((arrivals) => arrivals ?? [])
+    return await this.sqlite.withTransaction(async (db) => {
+      const results = await db.queryAll<
+        LocationCoordinate2D & {
+          arrivalRadiusMeters: number
+          eventId: number
+          hasArrived: number
+        }
+      >`
+      SELECT 
+        l.*, 
+        u.eventId AS eventId 
+      FROM UpcomingEventArrivals u
+      LEFT JOIN LocationArrivals l ON
+        u.latitude = l.latitude AND
+        u.longitude = l.longitude AND
+        u.arrivalRadiusMeters = l.arrivalRadiusMeters
+      ORDER BY 
+        u.latitude DESC, 
+        u.longitude DESC, 
+        u.arrivalRadiusMeters DESC
+      `
+      return results.reduce((acc, curr) => {
+        const region = {
+          coordinate: {
+            latitude: curr.latitude,
+            longitude: curr.longitude
+          },
+          arrivalRadiusMeters: curr.arrivalRadiusMeters,
+          isArrived: curr.hasArrived === 1
+        }
+        if (
+          acc.length > 0 &&
+          areEventRegionsEqual(acc[acc.length - 1], region)
+        ) {
+          acc[acc.length - 1].eventIds.push(curr.eventId)
+        } else {
+          acc.push({ eventIds: [curr.eventId], ...region })
+        }
+        return acc
+      }, [] as EventArrivalRegion[])
+    })
   }
 
   async replaceAll(regions: EventArrivalRegion[]) {
-    await AsyncStorage.setItem(
-      AsyncStorageUpcomingEventArrivals.KEY,
-      JSON.stringify(regions)
-    )
+    await this.sqlite.withTransaction(async (db) => {
+      await db.run`DELETE FROM LocationArrivals`
+      await Promise.all(
+        regions.map(async (region) => {
+          await db.run`
+          INSERT INTO LocationArrivals (
+            latitude, 
+            longitude, 
+            arrivalRadiusMeters, 
+            hasArrived
+          ) VALUES (
+            ${region.coordinate.latitude},
+            ${region.coordinate.longitude},
+            ${region.arrivalRadiusMeters},
+            ${region.isArrived ? 1 : 0}
+          )
+          `
+          await Promise.all(
+            region.eventIds.map(async (eventId) => {
+              await db.run`
+              INSERT INTO UpcomingEventArrivals (
+                eventId,
+                latitude,
+                longitude,
+                arrivalRadiusMeters
+              ) VALUES (
+                ${eventId},
+                ${region.coordinate.latitude},
+                ${region.coordinate.longitude},
+                ${region.arrivalRadiusMeters}
+              )`
+            })
+          )
+        })
+      )
+    })
   }
 }
 
---
+++ test-helpers/SQLite.ts
@@ -0,0 +1,17 @@
+import { TiFSQLite } from "@lib/SQLite"
+
+/**
+ * An in memory {@link TiFSQLite} instance for testing.
+ */
+export const testSQLite = new TiFSQLite(":memory:")
+
+/**
+ * Resets the data inside {@link testSQLite}.
+ */
+export const resetTestSQLiteBeforeEach = () => {
+  beforeEach(async () => {
+    await testSQLite.withTransaction(async (db) => {
+      await db.run`DELETE FROM LocationArrivals`
+    })
+  })
+}
