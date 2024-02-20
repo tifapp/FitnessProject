42af79de3b6e606661426650c0676e34d0563dc2
mhayes853 <matthewha853@gmail.com>
2024-02-20 00:49:57 -0800

    Migrate recent locations storage to SQLite

--- location/search/RecentsStorage.ts
+++ location/search/RecentsStorage.ts
@@ -1,13 +1,141 @@
-import { ArrayUtils } from "@lib/utils/Array"
-import { AsyncStorageUtils } from "@lib/utils/AsyncStorage"
-import AsyncStorage from "@react-native-async-storage/async-storage"
 import { z } from "zod"
-import {
-  LocationCoordinate2D,
-  TiFLocation,
-  TiFLocationSchema,
-  hashLocationCoordinate
-} from "@shared-models/Location"
+import { LocationCoordinate2D, TiFLocation } from "@shared-models/Location"
+import { TiFSQLite } from "@lib/SQLite"
+import { Placemark } from "@shared-models/Placemark"
+import { DeepNullable } from "@lib/utils/DeepNullable"
+
+/**
+ * An interface for storing locations that the user has interacted with
+ * previously.
+ */
+export interface RecentLocationsStorage {
+  /**
+   * Loads the `amount` most recent locations ordered by the time each location
+   * was last saved.
+   */
+  recent(amount: number): Promise<RecentLocation[]>
+
+  /**
+   * Loads recent locations which contain matching coordinates as any of the
+   * coordinates in the given array.
+   */
+  locationsForCoordinates(
+    coordinates: LocationCoordinate2D[]
+  ): Promise<RecentLocation[]>
+
+  /**
+   * Saves a location to the recents storage with the given annotation.
+   */
+  save(
+    location: TiFLocation,
+    annotation?: RecentLocationAnnotation
+  ): Promise<void>
+}
+
+/**
+ * {@link RecentLocationsStorage} implemented with SQLite.
+ */
+export class SQLiteRecentLocationsStorage implements RecentLocationsStorage {
+  private readonly sqlite: TiFSQLite
+
+  constructor(sqlite: TiFSQLite) {
+    this.sqlite = sqlite
+  }
+
+  async recent(amount: number) {
+    return await this.sqlite.withTransaction(async (db) => {
+      const results = await db.queryAll<SQLiteLocationPlacemark>`
+      SELECT * FROM LocationPlacemarks
+      ORDER BY recentUpdatedAt DESC
+      LIMIT ${amount}
+      `
+      return results.map(recentLocationFromSQLite)
+    })
+  }
+
+  async locationsForCoordinates(coordinates: LocationCoordinate2D[]) {
+    const stringifiedCoordinates = coordinates
+      .map((coordinate) => `${coordinate.latitude},${coordinate.longitude}`)
+      .join(",")
+    return await this.sqlite.withTransaction(async (db) => {
+      const results = await db.queryAll<SQLiteLocationPlacemark>`
+      SELECT * FROM LocationPlacemarks
+      WHERE ${stringifiedCoordinates} 
+        LIKE '%' || cast(latitude AS TEXT) || ',' || cast(longitude as TEXT) || '%'
+      ORDER BY recentUpdatedAt DESC
+      `
+      return results.map(recentLocationFromSQLite)
+    })
+  }
+
+  async save(location: TiFLocation, annotation?: RecentLocationAnnotation) {
+    await this.sqlite.withTransaction(async (db) => {
+      await db.run`
+      INSERT INTO LocationPlacemarks (
+        latitude,
+        longitude,
+        name,
+        country,
+        postalCode,
+        street,
+        streetNumber,
+        region,
+        isoCountryCode,
+        city,
+        recentAnnotation
+      ) VALUES (
+        ${location.coordinate.latitude},
+        ${location.coordinate.longitude},
+        ${location.placemark.name},
+        ${location.placemark.country},
+        ${location.placemark.postalCode},
+        ${location.placemark.street},
+        ${location.placemark.streetNumber},
+        ${location.placemark.region},
+        ${location.placemark.isoCountryCode},
+        ${location.placemark.city},
+        ${annotation}
+      ) 
+      ON CONFLICT(latitude, longitude)
+      DO UPDATE SET
+        name = ${location.placemark.name},
+        country = ${location.placemark.country},
+        postalCode = ${location.placemark.postalCode},
+        street = ${location.placemark.street},
+        streetNumber = ${location.placemark.streetNumber},
+        isoCountryCode = ${location.placemark.isoCountryCode},
+        city = ${location.placemark.city},
+        recentAnnotation = ${annotation},
+        recentUpdatedAt = unixepoch('now', 'subsec')
+      `
+    })
+  }
+}
+
+type SQLiteLocationPlacemark = LocationCoordinate2D &
+  DeepNullable<Placemark> & {
+    recentAnnotation: RecentLocationAnnotation | null
+  }
+
+const recentLocationFromSQLite = (location: SQLiteLocationPlacemark) => ({
+  location: {
+    coordinate: {
+      latitude: location.latitude,
+      longitude: location.longitude
+    },
+    placemark: {
+      name: location.name ?? undefined,
+      country: location.country ?? undefined,
+      postalCode: location.postalCode ?? undefined,
+      street: location.street ?? undefined,
+      streetNumber: location.streetNumber ?? undefined,
+      region: location.region ?? undefined,
+      isoCountryCode: location.isoCountryCode ?? undefined,
+      city: location.city ?? undefined
+    }
+  },
+  annotation: location.recentAnnotation ?? undefined
+})
 
 /**
  * A zod schema for {@link RecentLocationAnnotationSchema}.
@@ -30,90 +158,12 @@ export const RecentLocationAnnotationSchema = z.enum([
  * - `"hosted-event"`
  *    - Use this when the user creates an event
  */
-export type RecentLocationAnnotation = z.infer<
-  typeof RecentLocationAnnotationSchema
->
-
-/**
- * A zod schema for {@link RecentLocation}.
- */
-export const RecentLocationSchema = z.object({
-  location: TiFLocationSchema,
-  annotation: RecentLocationAnnotationSchema.optional()
-})
+export type RecentLocationAnnotation = "attended-event" | "hosted-event"
 
 /**
  * A type that contains recency data around a particular location.
  */
-export type RecentLocation = z.infer<typeof RecentLocationSchema>
-
-/**
- * Loads the specified amount of recent locations ordered by recency date descending.
- */
-export const asyncStorageLoadRecentLocations = async (amount: number) => {
-  const keylist = await loadRecentLocationsKeylist()
-  return await recentLocationsWithKeys(keylist.slice(0, amount))
-}
-
-/**
- * Loads specific recent locations from async storage based on the given coordinates.
- */
-export const asyncStorageLoadSpecificRecentLocations = async (
-  coordinates: LocationCoordinate2D[]
-) => {
-  return await recentLocationsWithKeys(
-    coordinates.map(recentLocationAsyncStorageKey)
-  )
-}
-
-const recentLocationsWithKeys = async (keys: string[]) => {
-  return await AsyncStorageUtils.parseJSONItems(
-    RecentLocationSchema,
-    keys
-  ).then((results) => {
-    return ArrayUtils.removeOptionals(results.map(([_, value]) => value))
-  })
-}
-
-/**
- * Saves a location to async storage with the given reason.
- */
-export const asyncStorageSaveRecentLocation = async (
-  location: TiFLocation,
+export type RecentLocation = {
+  location: TiFLocation
   annotation?: RecentLocationAnnotation
-) => {
-  const key = recentLocationAsyncStorageKey(location.coordinates)
-  const keylist = await loadRecentLocationsKeylist().then((keylist) =>
-    keylist.filter((keylistKey) => keylistKey !== key)
-  )
-
-  // NB: Time only moves forward, so we can keep the list sorted by simply just
-  // inserting at the start.
-  keylist.unshift(key)
-
-  await AsyncStorage.multiSet([
-    [RECENT_LOCATIONS_KEYLIST_KEY, JSON.stringify(keylist)],
-    [
-      key,
-      JSON.stringify({
-        location,
-        annotation
-      })
-    ]
-  ])
-}
-
-const RECENT_LOCATIONS_KEYLIST_KEY = "@recent_locations_keys"
-
-const RecentLocationsKeylistSchema = z.array(z.string())
-
-const loadRecentLocationsKeylist = async () => {
-  return await AsyncStorageUtils.parseJSONItem(
-    RecentLocationsKeylistSchema,
-    RECENT_LOCATIONS_KEYLIST_KEY
-  ).then((result) => result ?? [])
-}
-
-const recentLocationAsyncStorageKey = (coordinate: LocationCoordinate2D) => {
-  return `@location_${hashLocationCoordinate(coordinate)}`
 }
