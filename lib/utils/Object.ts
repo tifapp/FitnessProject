04099d9b09e4342dce4dfd934496b65571796b1a
mhayes853 <matthewha853@gmail.com>
2024-02-23 08:00:37 -0800

    Documentation

--- lib/utils/Object.ts
+++ lib/utils/Object.ts
@@ -1,4 +1,7 @@
 export namespace ObjectUtils {
+  /**
+   * Removes all keys that have a value of undefined from the given object.
+   */
   export const removeUndefined = <Obj extends { [key: string]: any }>(
     obj: Obj
   ) => {
