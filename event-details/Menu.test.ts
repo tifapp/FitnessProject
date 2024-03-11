850afdca0936f44068d1dcf7f15a4bc8aea993da
mhayes853 <matthewha853@gmail.com>
2024-03-10 20:56:23 -0700

    Toggle Block menu action

--- event-details/Menu.test.ts
+++ event-details/Menu.test.ts
@@ -1,6 +1,7 @@
 import { setPlatform } from "@test-helpers/Platform"
 import { EVENT_MENU_ACTION, formatEventMenuActions } from "./Menu"
 import { EventAttendeeMocks } from "./MockData"
+import { CurrentUserEvent } from "@shared-models/Event"
 
 describe("EventDetailsMenu tests", () => {
   describe("FormatEventMenuActions tests", () => {
@@ -8,7 +9,7 @@ describe("EventDetailsMenu tests", () => {
 
     it("should format the contact-host action with the host's name", () => {
       const actions = formatEventMenuActions(
-        { host: EventAttendeeMocks.Alivs },
+        { host: EventAttendeeMocks.Alivs } as CurrentUserEvent,
         [EVENT_MENU_ACTION.copyEvent, EVENT_MENU_ACTION.contactHost]
       )
       expect(actions).toEqual([
@@ -17,14 +18,47 @@ describe("EventDetailsMenu tests", () => {
       ])
     })
 
-    it("should reverse the actions if the platform is android", () => {
-      setPlatform("android")
+    it("should format the toggle-block-host with 'Block Hostname' when user is not blocking host", () => {
       const actions = formatEventMenuActions(
-        { host: EventAttendeeMocks.Alivs },
-        [EVENT_MENU_ACTION.copyEvent, EVENT_MENU_ACTION.editEvent]
+        { host: EventAttendeeMocks.Alivs } as CurrentUserEvent,
+        [EVENT_MENU_ACTION.toggleBlockHost]
       )
       expect(actions).toEqual([
-        EVENT_MENU_ACTION.editEvent,
+        {
+          ...EVENT_MENU_ACTION.toggleBlockHost,
+          title: "Block Alvis",
+          image: "person.slash.fill"
+        }
+      ])
+    })
+
+    it("should format the toggle-block-host with 'Unblock Hostname' when user is blocking host", () => {
+      const actions = formatEventMenuActions(
+        {
+          host: {
+            ...EventAttendeeMocks.Alivs,
+            relations: { themToYou: "not-friends", youToThem: "blocked" }
+          }
+        } as CurrentUserEvent,
+        [EVENT_MENU_ACTION.toggleBlockHost]
+      )
+      expect(actions).toEqual([
+        {
+          ...EVENT_MENU_ACTION.toggleBlockHost,
+          title: "Unblock Alvis",
+          image: "person.fill.checkmark"
+        }
+      ])
+    })
+
+    it("should reverse the actions if the platform is android", () => {
+      setPlatform("android")
+      const actions = formatEventMenuActions({} as CurrentUserEvent, [
+        EVENT_MENU_ACTION.copyEvent,
+        EVENT_MENU_ACTION.shareEvent
+      ])
+      expect(actions).toEqual([
+        EVENT_MENU_ACTION.shareEvent,
         EVENT_MENU_ACTION.copyEvent
       ])
     })
