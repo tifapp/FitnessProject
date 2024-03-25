d16f78326656f07d80c682adc97c7a76572c9771
mhayes853 <matthewha853@gmail.com>
2024-03-25 00:25:50 -0700

    Initial detox setup

---
+++ roswaal/basic-join-event-through-exploration/TestCase.test.ts
@@ -0,0 +1,27 @@
+// "Generated" by Roswaal, do not touch.
+import {
+  beforeLaunch,
+  attemptToMakeJohnnyJoinTheEvent,
+  ensureJohnnyHasSignedIntoHisAccount,
+  ensureJohnnysLocationIsInTheBayArea,
+  ensureThatJohnnyJoinedTheEventSuccessfully,
+  ensureTheDetailsMatchTheNearestEventInTheBayArea,
+  searchForEventsInTheBayAreaAndGoToTheDetailsForTheNearestOne
+} from "./TestActions"
+
+test("BasicJoinEventThroughExploration", async () => {
+  await beforeLaunch()
+  await device.launchApp()
+  // Johnny is signed in
+  await ensureJohnnyHasSignedIntoHisAccount()
+  // Johnny lives in the Bay Area
+  await ensureJohnnysLocationIsInTheBayArea()
+  // Johnny wants to find the nearest event in the Bay Area
+  await searchForEventsInTheBayAreaAndGoToTheDetailsForTheNearestOne()
+  // After finding an event, Johnny wants to scope out its details
+  await ensureTheDetailsMatchTheNearestEventInTheBayArea()
+  // Since Johnny is interested in the event, he now tries to join it
+  await attemptToMakeJohnnyJoinTheEvent()
+  // Johnny has now joined the event
+  await ensureThatJohnnyJoinedTheEventSuccessfully()
+})
