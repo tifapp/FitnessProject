3b43f06a57b406251142096d0cf8b4e83a69ec45
mhayes853 <matthewha853@gmail.com>
2024-03-25 00:25:50 -0700

    Initial detox setup

---
+++ roswaal/basic-join-event-through-exploration/TestActions.ts
@@ -0,0 +1,35 @@
+export const beforeLaunch = async () => {
+  // Perform any setup work in here, (setting location, reseting device
+  // permissions, etc.)
+}
+
+export const ensureJohnnyHasSignedIntoHisAccount = async () => {
+  // Johnny is signed in
+  throw new Error("TODO")
+}
+
+export const ensureJohnnysLocationIsInTheBayArea = async () => {
+  // Johnny lives in the Bay Area
+  throw new Error("TODO")
+}
+
+export const searchForEventsInTheBayAreaAndGoToTheDetailsForTheNearestOne =
+  async () => {
+    // Johnny wants to find the nearest event in the Bay Area
+    throw new Error("TODO")
+  }
+
+export const ensureTheDetailsMatchTheNearestEventInTheBayArea = async () => {
+  // After finding an event, Johnny wants to scope out its details
+  throw new Error("TODO")
+}
+
+export const attemptToMakeJohnnyJoinTheEvent = async () => {
+  // Since Johnny is interested in the event, he now tries to join it
+  throw new Error("TODO")
+}
+
+export const ensureThatJohnnyJoinedTheEventSuccessfully = async () => {
+  // Johnny has now joined the event
+  throw new Error("TODO")
+}
