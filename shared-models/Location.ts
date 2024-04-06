1dba4fd6661f2031a309fcd08565c9a95c110ed8
mhayes853 <matthewha853@gmail.com>
2024-04-05 23:20:12 -0700

    Use TiFShared Math'

--- shared-models/Location.ts
+++ shared-models/Location.ts
@@ -1,6 +1,5 @@
 import geohash from "ngeohash"
 import { z } from "zod"
-import { MathUtils } from "@lib/utils/Math"
 import { PlacemarkSchema } from "./Placemark"
 import { metersToMiles } from "@lib/utils/MetricConversions"
 
@@ -48,19 +47,19 @@ export const metersBetweenLocations = (
   location1: LocationCoordinate2D,
   location2: LocationCoordinate2D
 ) => {
-  const lat1Radians = MathUtils.degreesToRadians(location1.latitude)
-  const lat2Radians = MathUtils.degreesToRadians(location2.latitude)
+  const lat1Radians = Math.degreesToRadians(location1.latitude)
+  const lat2Radians = Math.degreesToRadians(location2.latitude)
 
-  const latDeltaRadians = MathUtils.degreesToRadians(
+  const latDeltaRadians = Math.degreesToRadians(
     location2.latitude - location1.latitude
   )
-  const lngDeltaRadians = MathUtils.degreesToRadians(
+  const lngDeltaRadians = Math.degreesToRadians(
     location2.longitude - location1.longitude
   )
 
-  const latDelta = MathUtils.sin2(latDeltaRadians / 2)
+  const latDelta = Math.sin2(latDeltaRadians / 2)
   const latCos = Math.cos(lat1Radians) * Math.cos(lat2Radians)
-  const lngDelta = MathUtils.sin2(lngDeltaRadians / 2)
+  const lngDelta = Math.sin2(lngDeltaRadians / 2)
 
   return (
     2 * EARTH_RADIUS_METERS * Math.asin(Math.sqrt(latDelta + latCos * lngDelta))
