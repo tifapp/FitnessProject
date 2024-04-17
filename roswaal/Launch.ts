a95c6c17862edd03e0f17f6c21a0fc55d6dd7f00
mhayes853 <matthewha853@gmail.com>
2024-04-16 18:38:56 -0700

    RawLaunchArguments

--- roswaal/Launch.ts
+++ roswaal/Launch.ts
@@ -1,12 +1,18 @@
-import { ConfigurableTestLaunchArguments } from "@lib/LaunchArguments"
+import { ConfigurableTestRawLaunchArguments } from "@lib/LaunchArguments"
 import { Reassign } from "TiFShared/lib/HelperTypes"
 import { device } from "detox"
 import { DeviceLaunchAppConfig } from "detox/detox"
 
+/**
+ * A configuration to launch the app in acceptance tests.
+ *
+ * With this configuration, it's possble to add launch arguments, override
+ * permissions, and much more.
+ */
 export type TestAppLaunchConfig = Reassign<
   DeviceLaunchAppConfig,
   "launchArgs",
-  ConfigurableTestLaunchArguments
+  ConfigurableTestRawLaunchArguments
 >
 
 /**
