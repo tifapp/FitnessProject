2d22a06be6697d626cd791165403109bb123b2a4
mhayes853 <matthewha853@gmail.com>
2024-05-09 15:54:10 -0700

    Initial screen

---
+++ settings-boundary/NotificationSettings.tsx
@@ -0,0 +1,11 @@
+import { StyleProp, ViewStyle } from "react-native"
+
+export type NotificationSettingsProps = {
+  style?: StyleProp<ViewStyle>
+}
+
+export const NotificationSettingsView = ({
+  style
+}: NotificationSettingsProps) => {
+  return <></>
+}
