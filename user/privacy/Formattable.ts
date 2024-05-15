a346a11f0182558beafac940fe694fcb17f23091
mhayes853 <matthewha853@gmail.com>
2024-05-14 17:28:07 -0700

    docs

--- user/privacy/Formattable.ts
+++ user/privacy/Formattable.ts
@@ -7,10 +7,17 @@ export interface PrivacyFormattable {
   get formattedForPrivacy(): string
 }
 
+/**
+ * An interface for describing how something can be stringified in a pretty way.
+ */
 export interface PrettyFormattable {
   get prettyFormatted(): string
 }
 
+/**
+ * An interface that requires a type to describe itself as a contact info type
+ * (email/phone number/etc.) in a pretty way.
+ */
 export interface ContactInfoTypeFormattable {
   get formattedContactInfoType(): string
   get contactInfoTypeIconName(): IoniconName
