580f6d31142261b1eb11111a9d111c36d0e092ba
mhayes853 <matthewha853@gmail.com>
2024-06-07 00:05:22 -0700

    Docs'

--- roswaal/TestCase.ts
+++ roswaal/TestCase.ts
@@ -16,6 +16,16 @@ export type RoswaalTestCaseActionResult = {
   didPass: boolean
 }
 
+/**
+ * A class modeling a test case in the roswaal tool.
+ *
+ * Test cases are constructed with a name and a setup function, then actions are appended to the
+ * test. When `run` is called, each action will be ran sequentially, and once either all actions
+ * finish, or if an error occurs in any action, then a progress report for the test case is
+ * returned.
+ *
+ * This class generally shouldn't be used outside of generated test code.
+ */
 export class RoswaalTestCase {
   private readonly actions = [] as (() => Promise<void>)[]
 
@@ -25,10 +35,23 @@ export class RoswaalTestCase {
     private readonly beforeLaunch: () => Promise<TestAppLaunchConfig>
   ) {}
 
+  /**
+   * Adds an action to this test case.
+   */
   appendAction(action: () => Promise<void>) {
     this.actions.push(action)
   }
 
+  /**
+   * Runs all actions sequentially until all are finished, or one errors, and returns an
+   * {@link RoswaalTestCaseResult}.
+   *
+   * If an action fails, then all other subsequent actions automatically are failed.
+   *
+   * @param launch A function to launch the app given a {@link TestAppLaunchConfig}.
+   * @returns A {@link RoswaalTestCaseResult}. The first action returned in the `actionResults`
+   * field is the result of the `beforeLaunch` action that was passed in the constructor.
+   */
   async run(
     launch: (config?: TestAppLaunchConfig) => Promise<void> = launchApp
   ): Promise<RoswaalTestCaseResult> {
