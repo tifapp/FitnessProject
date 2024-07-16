382cbaec783a4dc7420fcdf43f60157ebdeb9315
mhayes853 <75196016+mhayes853@users.noreply.github.com>
2024-07-15 18:26:03 -0700

    edit-event module (#345)

---
+++ edit-event/DurationPicker.tsx
@@ -0,0 +1,9 @@
+import { StyleProp, ViewStyle } from "react-native"
+
+export type EditEventDurationPickerProps = {
+  style?: StyleProp<ViewStyle>
+}
+
+export const EditEventDurationPickerView = ({
+  style
+}: EditEventDurationPickerProps) => <></>
