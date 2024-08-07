32cda3fec92c775006129894e000c04dd9870a34
roswaaltifbot <roswaaltifbot@gmail.com>
2024-08-07 03:03:52 +0000

    Roswaal: Add Tests "Roswaal can Siiiiiiiign In"

---
+++ roswaal/roswaal-can-siiiiiiiign-in/TestActions.ts
@@ -0,0 +1,22 @@
+import { TestAppLaunchConfig } from "../Launch"
+
+export const beforeLaunch = async (): Promise<TestAppLaunchConfig> => {
+  // Perform any setup work in here, (setting location, reseting device
+  // permissions, etc.)
+  return {}
+}
+
+export const haveRoswaalEnterHisNaaaaaaaaaame = async () => {
+  // Roswaal is at the doooooooooor.
+  throw new Error("TODO")
+}
+
+export const haveRoswaalEnterHisPaaaaaaaaassword = async () => {
+  // Roswaal is being blocked by securiiiiiiity.
+  throw new Error("TODO")
+}
+
+export const verifyThatRoswaalSignedInSucceeeeeeeessfully = async () => {
+  // Roswaal tries to access the ciiiiiiircus
+  throw new Error("TODO")
+}
