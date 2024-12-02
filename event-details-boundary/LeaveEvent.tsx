75d0974c0e912db1b5bfd8560bf73e5765887916
mhayes853 <matthewha853@gmail.com>
2024-12-02 02:12:47 -0800

    Update LeaveEvent

--- event-details-boundary/LeaveEvent.tsx
+++ event-details-boundary/LeaveEvent.tsx
@@ -1,9 +1,21 @@
 import { ClientSideEvent } from "@event/ClientSideEvent"
 import { updateEventDetailsQueryEvent } from "@event/DetailsQuery"
 import { AlertsObject, presentAlert } from "@lib/Alerts"
-import { useMutation, useQueryClient } from "@tanstack/react-query"
+import { useFormSubmission } from "@lib/utils/Form"
+import { useQueryClient } from "@tanstack/react-query"
+import { TiFAPI } from "TiFShared/api"
+import { EventID } from "TiFShared/domain-models/Event"
 
-export const LEAVE_EVENT_ALERTS = {
+export const leaveEvent = async (
+  id: EventID,
+  api: TiFAPI = TiFAPI.productionInstance
+): Promise<LeaveEventResult> => {
+  const resp = await api.leaveEvent({ params: { eventId: id } })
+  if (resp.status === 204 || resp.status === 200) return "success"
+  return resp.data.error
+}
+
+export const ALERTS = {
   "event-has-ended": {
     title: "Event has ended",
     description:
@@ -14,28 +26,31 @@ export const LEAVE_EVENT_ALERTS = {
     description:
       "This event was cancelled. You will be moved to the previous screen."
   },
-  "co-host-not-found": {
-    title: "Event has no co-host",
-    description:
-      "This event has no co-host. To leave, you will need to select a new host."
+  "event-not-found": {
+    title: "Event not Found",
+    description: "It seems that this event does not exist."
   },
   genericError: {
     title: "Uh-oh!",
     description: "Something went wrong. Please try again"
   },
-  confirmDelete: (deleteEvent?: () => void) => ({
-    title: "Delete Event",
-    description: "Are you sure you want to delete this event?",
+  confirmLeaveHost: (attendeeCount: number, onLeave?: () => void) => ({
+    title: "Leave this Event?",
+    description: `Are you sure about leaving the event? ${attendeeCount > 1 ? "The new host will be the person who first joined the event after you." : "Leaving will delete the event."}.`,
     buttons: [
-      {
-        text: "Delete",
-        style: "destructive",
-        onPress: deleteEvent
-      },
-      {
-        text: "Cancel",
-        style: "cancel"
-      }
+      { text: "Cancel" },
+      { text: "Leave", style: "destructive", onPress: onLeave }
+    ]
+  }),
+  confirmLeaveAttendee: (
+    host: ClientSideEvent["host"],
+    onLeave?: () => void
+  ) => ({
+    title: "Leave this Event?",
+    description: `Are you sure about leaving the event? ${host.name} (${host.handle}) would like you to stay if possible.`,
+    buttons: [
+      { text: "Cancel" },
+      { text: "Leave", style: "destructive", onPress: onLeave }
     ]
   })
 } satisfies AlertsObject
@@ -43,83 +58,59 @@ export const LEAVE_EVENT_ALERTS = {
 export type LeaveEventResult =
   | "success"
   | "event-has-ended"
+  | "event-not-found"
   | "event-was-cancelled"
-  | "co-host-not-found"
 
 export type UseLeaveEventEnvironment = {
   leaveEvent: (eventId: number) => Promise<LeaveEventResult>
   onSuccess: () => void
 }
 
-export type UseLeaveEventMenu =
-  | {
-      attendeeStatus: "hosting"
-      isLoading: boolean
-      deleteButtonTapped: () => void
-    }
-  | {
-      attendeeStatus: "attending"
-      isLoading: boolean
-      confirmButtonTapped: () => void
-    }
-
-export type UseLeaveEventStatus =
-  | { status: "success" }
-  | ({ status: "select" } & UseLeaveEventMenu)
-  | undefined
-
 export const useLeaveEvent = (
-  event: Pick<ClientSideEvent, "id" | "userAttendeeStatus">,
-  env: UseLeaveEventEnvironment
-): UseLeaveEventStatus => {
+  event: Pick<
+    ClientSideEvent,
+    "id" | "userAttendeeStatus" | "host" | "attendeeCount"
+  >,
+  { leaveEvent, onSuccess }: UseLeaveEventEnvironment
+) => {
   const queryClient = useQueryClient()
-  const { onSuccess, leaveEvent } = env
-  const leaveEventMutation = useMutation(
-    async () => await leaveEvent(event.id),
+  const submission = useFormSubmission(
+    (id: EventID) => leaveEvent(id),
+    () => {
+      if (event.userAttendeeStatus === "not-participating") {
+        return { status: "invalid" }
+      }
+      return { status: "submittable", submissionValues: event.id }
+    },
     {
       onSuccess: (status) => {
-        if (status === "co-host-not-found") {
-          presentAlert(LEAVE_EVENT_ALERTS[status])
-          updateEventDetailsQueryEvent(queryClient, event.id, (e) => ({
-            ...e,
-            userAttendeeStatus: "hosting"
-          }))
-        } else if (status === "success") {
+        if (status === "success") {
           onSuccess()
           updateEventDetailsQueryEvent(queryClient, event.id, (e) => ({
             ...e,
             userAttendeeStatus: "not-participating"
           }))
         } else {
-          presentAlert(LEAVE_EVENT_ALERTS[status])
+          presentAlert(ALERTS[status])
         }
       },
-      onError: () => presentAlert(LEAVE_EVENT_ALERTS.genericError)
+      onError: () => presentAlert(ALERTS.genericError)
     }
   )
-
-  const isSuccess =
-    leaveEventMutation.isSuccess && leaveEventMutation.data === "success"
-
-  if (isSuccess) {
-    return { status: "success" }
-  } else if (event.userAttendeeStatus === "attending") {
-    return {
-      status: "select",
-      isLoading: leaveEventMutation.isLoading,
-      attendeeStatus: event.userAttendeeStatus,
-      confirmButtonTapped: () => leaveEventMutation.mutate()
-    }
-  } else if (event.userAttendeeStatus === "hosting") {
-    return {
-      status: "select",
-      isLoading: leaveEventMutation.isLoading,
-      attendeeStatus: event.userAttendeeStatus,
-      deleteButtonTapped: () => {
-        presentAlert(
-          LEAVE_EVENT_ALERTS.confirmDelete(() => leaveEventMutation.mutate())
-        )
-      }
-    }
+  return {
+    leaveStarted:
+      submission.status === "submittable"
+        ? () => {
+            if (event.userAttendeeStatus === "attending") {
+              presentAlert(
+                ALERTS.confirmLeaveAttendee(event.host, submission.submit)
+              )
+            } else {
+              presentAlert(
+                ALERTS.confirmLeaveHost(event.attendeeCount, submission.submit)
+              )
+            }
+          }
+        : undefined
   }
 }
