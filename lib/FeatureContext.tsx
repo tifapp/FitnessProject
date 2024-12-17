8f868c049ec85bd44b674fb02c044446e816476d
mhayes853 <matthewha853@gmail.com>
2024-12-16 23:27:05 -0800

    Document featureContext

--- lib/FeatureContext.tsx
+++ lib/FeatureContext.tsx
@@ -5,6 +5,35 @@ export type FeatureContextProviderProps<
   ContextValues extends Record<string, any>
 > = Partial<ContextValues> & { children: ReactNode }
 
+/**
+ * Returns a context and a hook to use the context using our pattern for passing data functions
+ * to features via a context.
+ *
+ * Ex.
+ * ```ts
+ * export const MyCoolFeature = featureContext({
+ *   fetchData: async () => {
+ *     // Fetch...
+ *   }
+ * })
+ *
+ * export const MyCoolFeatureView = () => {
+ *   const { fetchData } = MyCoolFeature.useContext()
+ *   // Do stuff with data fn...
+ *   return <UI />
+ * }
+ *
+ * export const MockFeatureView = () => (
+ *  // Provide a fetch mock for the actual view for testing/storybook purposes.
+ *  <MyCoolFeature.Provider fetchData={mockFetch}>
+ *    <MyCoolFeatureView />
+ *  </MyCoolFeature>
+ * )
+ * ```
+ *
+ * @param initialValues The default values to pass to the context.
+ * @returns A provider component and a hook to consume the context.
+ */
 export const featureContext = <ContextValues extends Record<string, any>>(
   initialValues: ContextValues
 ) => {
