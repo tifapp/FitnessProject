de961e2cf8a7c4c099d2b5514680da9963412874
tifapp <tifapppublic@gmail.com>
2025-01-11 03:09:57 -0800

    add build to ci

--- AppEntry.js
+++ AppEntry.js
@@ -2,13 +2,14 @@ import { BUILD_TYPE } from "@env"
 import { registerRootComponent } from "expo"
 import "TiFShared"
 
-if (BUILD_TYPE !== "storybook") {
+if (BUILD_TYPE === "storybook") {
   // @ts-ignore App entry
-  const Module = require("./App")
-  Module.setupApp()
+  const Module = require("./.storybook/App")
   registerRootComponent(Module.default)
 } else {
   // @ts-ignore App entry
-  const Module = require("./.storybook/App")
+  const Module = require("./App")
+  // @ts-ignore Not inferring the type of "Module" correctly
+  Module.setupApp()
   registerRootComponent(Module.default)
 }
