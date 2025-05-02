facb12862e1780e1033b1a1c0c986610b0102093
ZoroarkZark <48034253+ZoroarkZark@users.noreply.github.com>
2025-05-02 14:55:12 -0700

    Fix CI error with AuthLayout redeclaration in imports

--- auth-boundary/AuthLayout.tsx
+++ auth-boundary/AuthLayout.tsx
@@ -15,8 +15,6 @@ import {
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view"
 import Animated from "react-native-reanimated"
 import { useSafeAreaInsets } from "react-native-safe-area-context"
-import { TiFFooterView } from "@components/Footer"
-import { TiFFormScrollableLayoutView } from "@components/form-components/ScrollableFormLayout"
 
 export type AuthSectionProps = {
   title: string
