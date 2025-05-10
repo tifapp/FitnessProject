e237eb7a136f63cc77497a252cc493c629f0eee2
tifapp <tifapppublic@gmail.com>
2025-05-09 23:22:51 -0700

    in eas pre build for the preview app, need to remove the storybook import.

--- easprebuild.mjs
+++ easprebuild.mjs
@@ -3,8 +3,31 @@ import fs from "fs"
 const { EAS_BUILD_TYPE } = process.env
 
 const directoryPath = "./.storybook"
+const appEntryPath = "./AppEntry.js"
 
-if (EAS_BUILD_TYPE !== "development" && fs.existsSync(directoryPath)) {
-  fs.rmdirSync(directoryPath, { recursive: true })
-  console.log(`${directoryPath} has been deleted`)
+// Remove storybook directory if not in development build
+if (EAS_BUILD_TYPE !== "development") {
+  if (fs.existsSync(directoryPath)) {
+    fs.rmdirSync(directoryPath, { recursive: true })
+    console.log(`${directoryPath} has been deleted`)
+  }
+
+  // Modify AppEntry.js to comment out the storybook require lines
+  if (fs.existsSync(appEntryPath)) {
+    let appEntryContent = fs.readFileSync(appEntryPath, "utf8")
+
+    // Comment out the two lines that reference storybook
+    appEntryContent = appEntryContent.replace(
+      /(const Module = require\("\.\/\.storybook\/App"\))/,
+      "// $1"
+    )
+    appEntryContent = appEntryContent.replace(
+      /(registerRootComponent\(Module\.default\))/,
+      "// $1"
+    )
+
+    // Write the modified content back
+    fs.writeFileSync(appEntryPath, appEntryContent)
+    console.log(`${appEntryPath} has been modified for production build`)
+  }
 }
